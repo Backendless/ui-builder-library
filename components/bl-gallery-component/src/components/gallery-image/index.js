76efa9a5af59d4a2d9e4364dafeba6476cffe981
addKiwi <skrypak1997@gmail.com>
2022-06-23 13:31:05 +0300

    fix gallery-image height

--- components/bl-gallery-component/src/components/gallery-image/index.js
+++ components/bl-gallery-component/src/components/gallery-image/index.js
@@ -2,7 +2,7 @@ export function GalleryImage({ url, height }) {
   return (
     <img
       src={ url }
-      style={{ height: height ? height : '' }}
+      style={{ height: height }}
       className="image"
     />
   );
