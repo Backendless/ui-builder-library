1f6a68bbc24a385116a5b913f7074b5fefdb209b
Zakhar Mychka <zakharii.mychka@plus4u.net>
2022-06-27 21:15:25 +0300

    fix error

---
+++ components/bl-rating-component/README.md
@@ -0,0 +1,30 @@
+# Rating
+Rating is a component of Backendless UI-Builder designer. It allows you to rate anything, or see the rating of a particular item.
+
+## Usage
+
+Add a component to your page and select the initial rating values.
+
+### Component Elements
+
+<dl>
+<dt>Icon</dt>
+<dd>Each icon has its own rating, when you click on it, the rating will change depending on the value of a particular icon.</dd>
+</dl>
+
+### Component Properties
+
+  Name              | Default value     | Description
+ -------------------|-------------------|-------------------------------------------------------------------------------------
+  Disabled          | false             | If you need the rating to be disabled, you can click on the `Disabled` checkbox.
+  Icon              | Star              | You can select the icon to be displayed.
+  Default value     | 0                 | You can select the initial value to be displayed.
+  Icons amount      | 5                 | This option is to set the number of icons in the rating.
+
+
+### Events
+
+<dl>
+<dt>On Rating change</dt>
+<dd>Triggers when the user clicks on the icon.</dd>
+</dl>
--- components/bl-rating-component/src/index.js
+++ components/bl-rating-component/src/index.js
@@ -1,79 +1,83 @@
-import { useCallback, useRef, useState } from 'react'
+import { useState, useRef, useMemo, useCallback } from 'react';
 
-import RatingIcon from './RatingIcon.js'
+import { RatingIcon } from './rating-icon.js';
+import { roundValueToPrecision } from './utils/number';
 
-function getDecimalPrecision(num) {
-  const decimalPart = num.toString().split('.')[1]
+export default function RatingComponent({ component, eventHandlers }) {
+  const { disabled, icon, defaultValue, iconsAmount, classList } = component;
+  const { onRatingChange } = eventHandlers;
 
-  return decimalPart ? decimalPart.length : 0
-}
+  const rootRef = useRef();
+  const [ratingValue, setRatingValue] = useState(defaultValue);
+  const [hoverValue, setHoverValue] = useState();
+  const iconsArray = new Array(iconsAmount).fill(1);
 
-function roundValueToPrecision(value, precision) {
-  if (value == null) return value
-
-  const nearest = Math.round(value / precision) * precision
-
-  return Number(nearest.toFixed(getDecimalPrecision(precision)))
-}
-
-export default function MyCustomComponent({ component, eventHandlers }) {
-  const { disabled, icon, defaultValue, iconsAmount, classList } = component
-  const { onRatingChange, onHighestRating } = eventHandlers
-
-  const rootRef = useRef()
-  const [ratingValue, setRatingValue] = useState(defaultValue)
-  const [hoverValue, setHoverValue] = useState()
-  const iconsArray = new Array(iconsAmount).fill(1)
-
-  const value = hoverValue || ratingValue
+  const value = hoverValue || ratingValue;
 
   const handleRatingValue = useCallback(({ target: { value } }) => {
-    setRatingValue(prevState => prevState === value ? 0 : value)
+    setRatingValue(prevState => prevState === value ? 0 : value);
+
     if (onRatingChange) {
-      onRatingChange({ curentRating: value })
+      onRatingChange({ curentRating: value });
+
+      if (value == iconsAmount) {
+        onRatingChange({ maxRating: value });
+      }
+
+      if (value == 0.5) {
+        onRatingChange({ maxRating: value });
+      }
     }
-    if (onHighestRating && value == iconsAmount) {
-      onHighestRating({ highestRating: value })
-    }
-  }, [])
 
-  const handleMouseMove = event => {
-    if (disabled) return
+  }, []);
 
-    const rootNode = rootRef.current
-    const { left } = rootNode.getBoundingClientRect()
-    const { width } = rootNode.firstChild.getBoundingClientRect()
+  const handleMouseMove = (event) => {
+    if (disabled) return;
 
-    const precision = 0.5
-    const percent = (event.clientX - left) / (width * iconsAmount)
+    const rootNode = rootRef.current;
+    const { left } = rootNode.getBoundingClientRect();
+    const { width } = rootNode.firstChild.getBoundingClientRect();
 
-    setHoverValue(roundValueToPrecision(iconsAmount * percent + precision / 2, precision).toString())
-  }
+    const precision = 0.5;
+    const percent = (event.clientX - left) / (width * iconsAmount);
+
+    setHoverValue(roundValueToPrecision(iconsAmount * percent + precision / 2, precision).toString());
+  };
 
   const handleMouseLeave = () => {
-    if (disabled) return
+    if (disabled) return;
 
-    setHoverValue()
-  }
+    setHoverValue();
+  };
+
+  const classes = useMemo(() => {
+    const classesArray = ["bl-customComponent-rating", ...classList];
+
+    if (disabled) {
+      classesArray.push('disabled');
+    }
+
+    return classesArray.join(' ');
+  }, [classList, disabled]);
 
   return (
-    <div className={ 'bl-customComponent-rating rating ' + classList.join(' ') }>
+    <div className={classes}>
       <div
-        ref={ rootRef }
-        onMouseMove={ handleMouseMove }
-        onMouseLeave={ handleMouseLeave }
-        className="rating__icons">
+        ref={rootRef}
+        onMouseMove={handleMouseMove}
+        onMouseLeave={handleMouseLeave}
+        className="bl-customComponent-rating__icons">
         {iconsArray.map((_, index) => (
           <RatingIcon
-            key={ index }
-            icon={ icon }
-            index={ index }
-            disabled={ disabled }
-            ratingValue={ value }
-            onChange={ handleRatingValue }
+            key={index}
+            icon={icon}
+            index={index}
+            disabled={disabled}
+            ratingValue={value}
+            onChange={handleRatingValue}
           />
         ))}
       </div>
     </div>
-  )
+  );
 };
