e7aaf8ad7666a174e4b51d65661b05e9c8efa088
EXPORTER1700 <i.stetsenko1998@gmail.com>
2022-06-28 15:52:52 +0300

    add index file for button

---
+++ components/bl-webcamPhoto-component/src/buttons/index.js
@@ -0,0 +1,11 @@
+import { DoneButton } from './done';
+import { MakePhotoButton } from './make-photo';
+import { MakeSnapshotButton } from './make-snapshot';
+import { UploadButton } from './upload';
+
+export {
+  DoneButton,
+  MakePhotoButton,
+  MakeSnapshotButton,
+  UploadButton
+};
