fd62f6022066e88ad26836dc1e1338fb2d9e0b26
Zakhar Mychka <zakharii.mychka@plus4u.net>
2022-07-08 11:26:10 +0300

    remove unnecessary files

--- components/bl-transferList-component/src/components/Checkbox.js
+++
@@ -1,8 +0,0 @@
-import { Icon, Input } from './';
-
-export const Checkbox = ({ icon, iconColor, ...inputProps }) => (
-  <div className="checkbox__container">
-    <Input props={inputProps} />
-    <Icon icon={icon} color={iconColor} />
-  </div>
-);
