d6dda0dd2223f37fa36efc861e9f951c60b287de
Vladimir Upirov <4648606+Valodya@users.noreply.github.com>
2022-07-12 13:38:10 +0300

    - add "custom html" Components Template

---
+++ components/templates/src/custom-html/src/index.js
@@ -0,0 +1,27 @@
+import { useRef, useEffect } from 'react'
+
+const customHTML = (`
+    <input type="text" class="my-custom-input" placeholder="input anny text and click on the button"/>
+    <button class="my-custom-button">My Custom Button</button>  
+`)
+
+export default function CustomHTMLComponent({ component }) {
+  const elRef = useRef(null)
+
+  useEffect(() => {
+    const $el = elRef.current
+
+    $el.innerHTML = customHTML
+
+    const $input = $el.querySelector('input')
+    const $button = $el.querySelector('button')
+
+    $button.addEventListener('click', () => {
+      alert('Alert from a custom component: ' + $input.value)
+    })
+  }, [])
+
+  return (
+    <div ref={ elRef } className={ `my-custom-component-container ${ component.classList.join(' ') }` }/>
+  )
+}
