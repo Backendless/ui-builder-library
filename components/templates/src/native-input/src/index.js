f53e446e7f4348493f99baf451e32c47459fd119
Vladimir Upirov <4648606+Valodya@users.noreply.github.com>
2022-07-12 13:44:03 +0300

    - add "native-input" Components Template

---
+++ components/templates/src/native-input/src/index.js
@@ -0,0 +1,17 @@
+export default function NativeInput({ component, eventHandlers }) {
+  const onChange = e => {
+    const value = e.target.value
+
+    component.value = value
+
+    eventHandlers.onChange({ value })
+  }
+
+  return (
+    <input
+      className={ component.classList.join(' ') }
+      value={ component.value || '' }
+      onChange={ onChange }
+    />
+  )
+}
