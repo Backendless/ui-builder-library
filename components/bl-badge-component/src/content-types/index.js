9399df15c026ac289f3283cc83d01b005a29bc8b
Sergiy Melnychuk <serh.mel@gmail.com>
2022-07-20 18:54:58 +0300

    change the value type for the icon property and restructure files

--- components/bl-badge-component/src/content-types/index.js
+++
@@ -1,3 +0,0 @@
-export * from './icon';
-export * from './image';
-export * from './text';
--- components/bl-badge-component/src/index.js
+++ components/bl-badge-component/src/index.js
@@ -4,14 +4,13 @@ import { Content } from './content';
 export default function BadgeComponent({ component, eventHandlers }) {
   const { display, classList, padding } = component;
 
+
   if (!display) {
     return null;
   }
-
+  
   return (
-    <div
-      className={ 'bl-customComponent-badge ' + classList.join(' ') }
-      style={{ padding }}>
+    <div className={ 'bl-customComponent-badge ' + classList.join(' ') } style={{ padding }}>
       <Content component={ component } eventHandlers={ eventHandlers }/>
       <Badge component={ component } eventHandlers={ eventHandlers }/>
     </div>
