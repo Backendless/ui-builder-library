df5b52b77bc2cf2170014729a624c0f6b5ab0808
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-07-28 15:18:35 +0200

    BKNDLSS-29093 Custom HTML (#17)
    * create CustomHtml component and write README.md
    
    * add faIcon
    
    * fix of comments

---
+++ components/bl-customHtml-component/src/index.js
@@ -0,0 +1,27 @@
+import { useState } from 'react';
+
+export default function CustomHtml({ component }) {
+  const { display, html, classList } = component;
+
+  const [rawHtml, setRawHtml] = useState(html);
+
+  const classes = useClasses(classList);
+
+  component.setHtml = (html) => {
+    setRawHtml(html);
+  };
+
+  if (!display) {
+    return null;
+  }
+
+  return (
+    <div className={ classes } dangerouslySetInnerHTML={ { __html: rawHtml } }></div>
+  );
+}
+
+const useClasses = (classList) => {
+  const classes = ['bl-customComponent-customHtml', ...classList];
+
+  return classes.join(' ');
+};
