d28e8ad05640d84624ebb82c66267cd15376f5fe
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-08-03 11:51:09 +0200

    create component lottie animation and write README.md

---
+++ components/bl-lottieAnimation-component/README.md
@@ -0,0 +1,43 @@
+# Lottie Animation
+
+Lottie Animation is a component of Backendless UI-Builder designer. This allows you to upload and add animation in your page.
+
+The component based on external [Lottie Animation](https://github.com/airbnb/lottie-web).
+## Usage
+
+### Styles and Settings
+
+<dl>
+<dt>Data</dt>
+<dd>Allows write json for animation.</dd>
+<dt>Type</dt>
+<dd>Allows select type of animation (svg, canvas, Html). Default value 'svg'.</dd>
+<dt>Loop</dt>
+<dd>Allows to specify looping or non-looping animation. Default value 'true'</dd>
+<dt>Stopped</dt>
+<dd>Allows to specify stopped or not stopped animation.</dd>
+</dl>
+
+### Event Handlers and Bindable Properties
+
+<dl>
+<dt>Data Logic</dt>
+<dd>Allows to specify data for animation in Backendless logic</dd>
+<dt>Loop Logic</dt>
+<dd>Allows to specify looping or non-looping animation for in Backendless logic</dt>
+<dt>Stopped Logic</dt>
+<dd>Allows to specify stopped or not stopped animation in Backendless logic</dd>
+<dt>On Hover</dt>
+<dd>Triggered when the user hovers on an animation.</dd>
+<dt>On Unhover</dt>
+<dd>Triggered when the user moves the cursor away from the animation.</dd>
+</dl>
+
+### Action
+<dl>
+<dt>Stop Animation</dt>
+<dd>An action that stop animation</dd>
+<dt>Play Animation</dt>
+<dd>An action that play animation</dd>
+</dl>
+
---
+++ components/bl-lottieAnimation-component/src/index.js
@@ -0,0 +1,60 @@
+import { useEffect, useRef } from 'react';
+import { destroy, loadAnimation, play, stop } from './lib/lottie.min';
+
+const { cn } = BackendlessUI.CSSUtils;
+
+export default function lottieAnimation({ component, eventHandlers }) {
+  const { display, classList, data, isLoop, type, isStopped } = component;
+  const { onHover, onUnhover } = eventHandlers;
+
+  const container = useRef(null);
+
+  useEffect(() => {
+    loadAnimationHandler(container, type, isLoop, data);
+
+    if (isStopped) {
+      stop();
+    }
+  }, [data]);
+
+  component.playAnimation = () => {
+    play();
+  };
+
+  component.stopAnimation = () => {
+    stop();
+  };
+
+  if (!display) {
+    return null;
+  }
+
+  return (
+    <div
+      className={ cn('bl-customComponent-lottieAnimation', classList) }
+      ref={ container }
+      onMouseEnter={ onHover }
+      onMouseLeave={ onUnhover }></div>
+  );
+}
+
+const loadAnimationHandler = (container, type, isLoop, data) => {
+  destroy();
+
+  if (data) {
+    let animationJson;
+
+    if (data != null && data.constructor.name === 'Object') {
+      animationJson = data;
+    } else if (typeof data === 'string') {
+      animationJson = JSON.parse(data);
+    }
+
+    loadAnimation({
+      container    : container.current,
+      renderer     : type,
+      loop         : isLoop,
+      animationData: animationJson
+    });
+  }
+};
