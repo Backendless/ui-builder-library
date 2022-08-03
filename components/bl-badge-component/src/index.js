0e61ae60cca0dc25cdbc02300eea0cf32350b191
Sergiy Melnychuk <serh.mel@gmail.com>
2022-08-03 13:16:11 +0300

    use cn for className and remove .gitignore

--- components/bl-badge-component/src/index.js
+++ components/bl-badge-component/src/index.js
@@ -1,6 +1,8 @@
 import { Badge } from './badge';
 import { Content } from './content';
 
+const { cn } = BackendlessUI.CSSUtils;
+
 export default function BadgeComponent({ component, eventHandlers }) {
   const { display, classList, padding } = component;
 
@@ -9,7 +11,7 @@ export default function BadgeComponent({ component, eventHandlers }) {
   }
   
   return (
-    <div className={ 'bl-customComponent-badge ' + classList.join(' ') } style={{ padding }}>
+    <div className={ cn(classList, 'bl-customComponent-badge') } style={{ padding }}>
       <Content component={ component } eventHandlers={ eventHandlers }/>
       <Badge component={ component } eventHandlers={ eventHandlers }/>
     </div>
