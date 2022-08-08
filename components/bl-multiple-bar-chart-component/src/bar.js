2e762c12dead9ff61a012317509578094a264939
Zakhar Mychka <zakharii.mychka@plus4u.net>
2022-08-08 16:06:36 +0300

    refactoring, added grid properties

--- components/bl-multiple-bar-chart-component/src/bar.js
+++ components/bl-multiple-bar-chart-component/src/bar.js
@@ -1,23 +1,18 @@
-import { decorateNumber } from './decorate-number';
+import { useChartData } from './use-chart-data';
 
 export function Bar({ name, goal, progress }) {
-  const barFillPercent = {
-    width: `${ (progress / (goal / 100)).toFixed(2) }%`,
-  };
-  
-  const decoratedGoal = decorateNumber(String(goal));
-  const decoratedProgress = decorateNumber(String(progress));
-  
+  const { shownGoal, barFillStyle, shownProgress } = useChartData(goal, progress);
+
   return (
     <div className="chart__item">
       <div className="chart__name-wrap">
         <a className="chart__name" href="#">{ name }</a>
       </div>
       <div className="chart__bar">
-        <div className="chart__bar-fill" style={ barFillPercent }></div>
+        <div className="chart__bar-fill" style={ barFillStyle }></div>
         <div class="chart__bar-info">
           <div class="chart__bar-info-triangle" />
-          <div class="chart__bar-info-rectangle">{ `${decoratedProgress} / ${decoratedGoal}` }</div>
+          <div class="chart__bar-info-rectangle">{ `${ shownProgress } / ${ shownGoal }` }</div>
         </div>
       </div>
     </div>
--- components/bl-multiple-bar-chart-component/src/index.js
+++ components/bl-multiple-bar-chart-component/src/index.js
@@ -1,15 +1,16 @@
 import { useState, useEffect } from 'react';
 
-import { validate } from './validate';
 import { Bar } from './bar';
+import { validate } from './validate';
 
 const { cn } = BackendlessUI.CSSUtils;
-const percentageMarksList = [0, 25, 50, 75, 100];
 
 export default function MultipleBarChartComponent({ component }) {
-  const { disable, classList, data } = component;
+  const { classList, display, disable, data, displayGrid, gridMarks } = component;
 
   const [chartData, setChartData] = useState([]);
+
+  const marksList = gridMarks.split(', ');
 
   useEffect(() => {
     setChartData(validate(data));
@@ -23,25 +24,29 @@ export default function MultipleBarChartComponent({ component }) {
     <div className={ cn('bl-customComponent-multipleBarChart', ...classList, { disable }) }>
       <div className="chart">
         <div className="chart__items">
-          <div className="chart__grids">
-            { percentageMarksList.map(item => (
-              <div className="chart__y-grid-line" style={{ left: `${item}%`}} />
-            )) }
-          </div>
+          { displayGrid &&
+            <div className="chart__grids">
+              { marksList.map(item => (
+                <div className="chart__y-grid-line" style={{ left: `${ item }%`}} />
+              )) }
+            </div>
+          }
           { chartData.length > 0 && chartData.map(({ objectId, name, goal, progress }) => (
             <Bar
               key={ objectId }
-              name={ name ? name : 'Name' }
+              name={ name || 'Name' }
               goal={ goal ? goal : 1 }
-              progress={ progress ? progress : 0 }
+              progress={ progress || 0 }
             />
           )) }
         </div>
-        <div className="chart__percentage-marks">
-          { percentageMarksList.map(item => (
-            <div className="chart__percentage-marks-item" style={{ left: `${item}%`}}>{ `${item}%` }</div>
-          )) }
-        </div>
+        { displayGrid &&
+          <div className="chart__percentage-marks">
+            { marksList.map(item => (
+              <div className="chart__percentage-marks-item" style={{ left: `${ item }%`}}>{ `${ item }%` }</div>
+            )) }
+          </div>
+        }
       </div>
     </div>
   );
