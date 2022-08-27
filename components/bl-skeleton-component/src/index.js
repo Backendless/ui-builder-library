248b939d3051dd2e224235f0c05e47bb3032aeeb
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-08-27 12:25:28 +0200

    add property "style"

--- components/bl-skeleton-component/src/index.js
+++ components/bl-skeleton-component/src/index.js
@@ -1,7 +1,7 @@
 const { cn } = BackendlessUI.CSSUtils;
 
 export default function Skeleton({ component }) {
-  const { display, classList, variant, width, height, animation } = component;
+  const { style, display, classList, variant, width, height, animation } = component;
 
   if (!display) {
     return null;
@@ -10,7 +10,7 @@ export default function Skeleton({ component }) {
   return (
     <div
       className={ cn('bl-customComponent-skeleton', `${ variant }`, `${ animation }`, classList) }
-      style={ { width: width ? width : '100%', height: height ? height : '100%' } }>
+      style={ { ...style, width: width ? width : '100%', height: height ? height : '100%' } }>
     </div>
   );
 }
