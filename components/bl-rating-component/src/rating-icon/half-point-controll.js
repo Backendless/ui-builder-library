09009dbce71952252717be959074c3ac8faab7b4
Zakhar Mychka <zakharii.mychka@plus4u.net>
2022-09-16 14:30:33 +0300

    bug fix

--- components/bl-rating-component/src/rating-icon/half-point-controll.js
+++ components/bl-rating-component/src/rating-icon/half-point-controll.js
@@ -1,28 +1,32 @@
-export const HalfPointControll = ({ icon, iconColor, disabled, checked, inputValue, onChange }) => (
-  <>
-    <label
-      htmlFor={ inputValue }
-      className="rating-icon__label half-icon">
-      <span className="rating-icon__span">
-        <svg
-          focusable="false"
-          aria-hidden="true"
-          viewBox="0 0 24 24"
-          fill={ iconColor }
-          className="rating-icon__svg">
-          <path d={ icon } />
-        </svg>
-      </span>
-    </label>
-    <input
-      type="radio"
-      name="rating"
-      value={ inputValue }
-      id={ inputValue }
-      disabled={ disabled }
-      checked={ checked }
-      className="rating-icon__input"
-      onChange={ onChange }
-    />
-  </>
-);
+export const HalfPointControll = ({ icon, iconColor, disabled, checked, inputValue, onChange }) => {
+  const id = BackendlessUI.UUID.short();
+
+  return (
+    <>
+      <label
+        htmlFor={ id }
+        className="rating-icon__label half-icon">
+        <span className="rating-icon__span">
+          <svg
+            focusable="false"
+            aria-hidden="true"
+            viewBox="0 0 24 24"
+            fill={ iconColor }
+            className="rating-icon__svg">
+            <path d={ icon } />
+          </svg>
+        </span>
+      </label>
+      <input
+        type="radio"
+        name="rating"
+        value={ inputValue }
+        id={ id }
+        disabled={ disabled }
+        checked={ checked }
+        className="rating-icon__input"
+        onChange={ onChange }
+      />
+    </>
+  );
+}
