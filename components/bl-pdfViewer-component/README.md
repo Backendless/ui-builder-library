e6cc8b816b95464a89afb947d0c8a25046a7793a
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-09-23 19:09:29 +0200

    rework size logic and minor fixes

--- components/bl-pdfViewer-component/README.md
+++ components/bl-pdfViewer-component/README.md
@@ -6,25 +6,26 @@ The component allows you to add a pdf viewer to your UI-Builder application
 
 ## Properties
 
-| Property   | Type                               | Default Value | Logic             | Data Binding | UI Setting | Description                                                          |
-|------------|------------------------------------|---------------|-------------------|--------------|------------|----------------------------------------------------------------------|
-| pdfUrl     | String                             |               | PDF URL Logic     | YES          | YES        | Allows write URL of PDF file, but file must be in Backendless Files. |
-| renderType | Select ["canvas","svg"]            | "canvas"      | Render Type Logic | NO           | YES        | Allows select type of render(`canvas`, `svg`).                       |
-| size       | Select ["small","normal", "large"] | "normal"      | Size Logic        | NO           | YES        | Allows select size of PDF Viewer(`small`, `normal`, `large`).        |
+| Property   | Type                    | Default Value | Logic             | Data Binding | UI Setting | Description                                                          |
+|------------|-------------------------|---------------|-------------------|--------------|------------|----------------------------------------------------------------------|
+| pdfUrl     | String                  |               | PDF URL Logic     | YES          | YES        | Allows write URL of PDF file, but file must be in Backendless Files. |
+| renderType | Select ["canvas","svg"] | "canvas"      | Render Type Logic | NO           | YES        | Allows select type of render(`canvas`, `svg`).                       |
+| width      | String                  | "0px"         | Width Logic       | YES          | YES        | Allows determine the width of the PDF Viewer.                        |
+| height     | String                  | "0px"         | Height Logic      | YES          | YES        | Allows determine the height of the PDF Viewer.                       |
 
 ## Events
 
-| Name            | Triggers                                     | Context Blocks      |
-|-----------------|----------------------------------------------|---------------------|
-| On Load Success | when the PDF file is successfully downloaded | `Num Pages: number` |
-| On Load Error   | when a PDF download error                    | `Message: string`   |
+| Name            | Triggers                                     | Context Blocks       |
+|-----------------|----------------------------------------------|----------------------|
+| On Load Success | when the PDF file is successfully downloaded | Page Count: `Number` |
+| On Load Error   | when a PDF download error                    | Message: `String`    |
 
 
 ## Action
 
-| Action     | Inputs         | Returns |
-|------------|----------------|---------|
-| Go To Page | `page: number` |         |
+| Action   | Inputs         | Returns |
+|----------|----------------|---------|
+| Set Page | page: `Number` |         |
 
 ## Styles
 
@@ -38,5 +39,6 @@ The component allows you to add a pdf viewer to your UI-Builder application
 ````
 @bl-customComponent-pdfViewer-input-background-color: if(@isLightTheme, darken(#fff, 10%), lighten(#000, 15%));
 @bl-customComponent-pdfViewer-button-icon-size: 16px;
+@bl-customComponent-pdfViewer-button-icon-color: contrast(@buttonContainedBackground);
 @bl-customComponent-pdfViewer-no-data-icon-size: 40px;
 ````
--- components/bl-pdfViewer-component/src/index.js
+++ components/bl-pdfViewer-component/src/index.js
@@ -1,17 +1,11 @@
 import { Document, Page } from './react-pdf.min.js';
 import { useState, useEffect, useRef } from 'react';
-import { Controls } from './subcomponents';
+import { Controls, NoData } from './subcomponents';
 
 const { cn } = BackendlessUI.CSSUtils;
 
-const sizeVariants = {
-  'small' : { minHeight: 385, width: 250 },
-  'normal': { minHeight: 550, width: 350 },
-  'large' : { minHeight: 630, width: 400 },
-};
-
 export default function PdfViewer({ component, eventHandlers }) {
-  const { style, display, classList, pdfUrl, renderType, size } = component;
+  const { style, display, classList, pdfUrl, renderType, width, height } = component;
   const { onLoadSuccess, onLoadError } = eventHandlers;
 
   const [numPages, setNumPages] = useState(null);
@@ -27,41 +21,29 @@ export default function PdfViewer({ component, eventHandlers }) {
 
   useEffect(() => {
     if (documentRef) {
-      documentRef.style = `height: ${ sizeVariants[size].minHeight }px`;
+      documentRef.style.height = height;
+      documentRef.style.width = width;
     }
   }, [documentRef]);
 
   const onDocumentLoadSuccess = ({ numPages }) => {
     setNumPages(numPages);
-    onLoadSuccess({ numPages });
+    onLoadSuccess({ pageCount: numPages });
   };
 
   const onDocumentLoadError = (error) => {
     onLoadError({ message: error.message });
   };
 
+  const onPageLoadSuccess = () => {
+    documentRef.firstChild.firstChild.style.width = width;
+    documentRef.firstChild.firstChild.style.height = height;
+  };
+
   const onNoData = () => {
     setControlsDisabled(true);
 
-    return (
-      <>
-        <h3>No PDF file specified.</h3>
-        <svg className="no-data-icon" width="24" height="24" viewBox="0 0 24 24">
-          <path
-                d="M12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4ZM2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22C6.47715 22 2 17.5228 2 12Z"
-                fill="black"/>
-          <path
-            d="M11 9.5C11 10.3284 10.3284 11 9.5 11C8.67157 11 8 10.3284 8 9.5C8 8.67157 8.67157 8 9.5 8C10.3284 8 11 8.67157 11 9.5Z"
-            fill="black"/>
-          <path
-            d="M16 9.5C16 10.3284 15.3284 11 14.5 11C13.6716 11 13 10.3284 13 9.5C13 8.67157 13.6716 8 14.5 8C15.3284 8 16 8.67157 16 9.5Z"
-            fill="black"/>
-          <path
-                d="M11.6095 14.9282C10.7456 15.0454 10.0166 15.4544 9.64021 15.768C9.21593 16.1216 8.58537 16.0643 8.2318 15.64C7.87824 15.2157 7.93556 14.5851 8.35984 14.2316C8.98348 13.7119 10.0544 13.1209 11.3406 12.9464C12.6643 12.7668 14.203 13.0339 15.6402 14.2316C16.0645 14.5851 16.1218 15.2157 15.7682 15.64C15.4147 16.0643 14.7841 16.1216 14.3598 15.768C13.3971 14.9657 12.4357 14.8161 11.6095 14.9282Z"
-                fill="black"/>
-        </svg>
-      </>
-    );
+    return <NoData/>;
   };
 
   const handlerPageChange = ({ target }) => {
@@ -84,7 +66,7 @@ export default function PdfViewer({ component, eventHandlers }) {
     }
   };
 
-  component.goPage = (page) => {
+  component.setPage = (page) => {
     setPageIndex(page);
   };
 
@@ -95,7 +77,7 @@ export default function PdfViewer({ component, eventHandlers }) {
   return (
     <div
       className={ cn('bl-customComponent-pdfViewer', classList) }
-      style={ { ...style, ...sizeVariants[size] } }>
+      style={ { ...style } }>
       <Document
         inputRef={ (ref) => setDocumentRef(ref) }
         className="pdf-viewer"
@@ -106,8 +88,7 @@ export default function PdfViewer({ component, eventHandlers }) {
         onLoadSuccess={ onDocumentLoadSuccess }>
         <Page
           renderTextLayer={ false }
-          width={ sizeVariants[size].width }
-          height={ sizeVariants[size].minHeight }
+          onLoadSuccess={ onPageLoadSuccess }
           pageNumber={ pageIndex }
         />
       </Document>
