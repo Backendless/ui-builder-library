a4446cafa7d23c3f15e169caf4d9b81f71f61bbf
Roman Skrypak <skrypak1997@gmail.com>
2022-09-28 10:14:23 +0300

    BKNDLSS-29803 Drag and drop list (#101)
    * BKNDLSS-29803
    added displaying "value" property
    
    * added checkbox to determine if show the value of item, added to README.md showValues in Properties

--- components/bl-draggable-list-component/src/list-item.js
+++ components/bl-draggable-list-component/src/list-item.js
@@ -45,7 +45,12 @@ export function ListItem({ item, onDragStart, onDragEnd, onDragOver, index, comp
         <DraggableIcon/>
         { isEdit
           ? <EditControl item={ itemState } onChange={ handleChange }/>
-          : <span>{ item.label }</span>
+          : <div>
+              <span style={ { 'display': 'inline-block', 'min-width': '100px' } }>{ item.label }</span>
+              { showValues && (
+                <span style={ { 'margin-left': '10px' } }>{ item.value }</span>
+              ) }
+            </div>
         }
       </div>
       <ItemControls
