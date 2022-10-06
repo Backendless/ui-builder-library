0bd19ab3b7e5534d4e91d8015c381d7e26fe307a
Roman Skrypak <skrypak1997@gmail.com>
2022-10-06 10:51:22 +0300

    BKNDLSS-29876 Leaflet Map (#106)
    * BKNDLSS-29876
    added possibility to take "POINT" type from DB
    
    * update object checking

--- components/bl-leafletMap-component/src/helpers/coordinates.js
+++ components/bl-leafletMap-component/src/helpers/coordinates.js
@@ -1,12 +1,18 @@
-const regEx = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
+const POINT_REGEX = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/;
 
 export function toCoordinates(coords) {
-  if (Array.isArray(coords) && regEx.test(coords.join(','))) {
+  if (Array.isArray(coords) && POINT_REGEX.test(coords.join(','))) {
     return coords;
   }
 
-  if (typeof coords === 'string' && regEx.test(coords)) {
+  if (typeof coords === 'string' && POINT_REGEX.test(coords)) {
     return coords.split(',');
+  }
+
+  if (typeof coords === 'object' && coords.center) {
+    if (POINT_REGEX.test(`${ coords.center.lat },${ coords.center.lng }`)) {
+      return [coords.center.lat, coords.center.lng];
+    }
   }
 
   console.error('Leaflet Map: not valid coordinates');
