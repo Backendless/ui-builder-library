aaefe14fda731636ecef3f5bad44dd9fc997f28e
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-10-14 20:07:07 +0300

    added setUrl and setContent action, refactored condition rendering, optimized md variable, minor fix

--- components/bl-markdown-component/src/index.js
+++ components/bl-markdown-component/src/index.js
@@ -1,5 +1,6 @@
+import { useEffect, useMemo, useState } from 'react';
+
 import MarkdownIt from './markdown-it.min.js';
-import { useState, useEffect } from 'react';
 
 const { cn } = BackendlessUI.CSSUtils;
 
@@ -7,29 +8,45 @@ export default function MyCustomComponent({ component }) {
   const { classList, style, display, url, markdownText, height, width } = component;
 
   const [markdown, setMarkdown] = useState('');
-  const [isLoad, setIsLoad] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
   const [errorMessage, setErrorMessage] = useState('');
 
+  const mdUrl = useMemo(() => url, [url]);
+
+  const md = useMemo(() => new MarkdownIt(), []);
+
+  component.setUrl = (url) => {
+    loadMd(url);
+  };
+
+  component.setContent = (text) => {
+    loadMd(mdUrl, text);
+  };
+
   useEffect(() => {
-    const md = new MarkdownIt();
-    if (!markdownText && url) {
-      setIsLoad(true);
+    loadMd(mdUrl, markdownText);
+  }, [markdownText, mdUrl]);
+
+  const loadMd = (url, text) => {
+    if (!text && url) {
+      setIsLoading(true);
+
       fetch(url)
         .then(response => response.text())
         .then((data) => {
-          setIsLoad(false);
           setMarkdown(md.render(data));
+          setIsLoading(false);
         })
         .catch((error) => {
-          setIsLoad(false);
           setErrorMessage(error.message);
+          setIsLoading(false);
         });
-    } else if (markdownText) {
-      setMarkdown(md.render(markdownText));
+    } else if (text) {
+      setMarkdown(md.render(text));
     } else {
       setMarkdown('<h1>No specified markdown text</h1>');
     }
-  }, [markdownText, url]);
+  };
 
   if (!display) {
     return null;
@@ -39,20 +56,26 @@ export default function MyCustomComponent({ component }) {
     <div
       className={ cn('bl-customComponent-markdown markdown-body', classList) }
       style={ { ...style, height: height || '100%', width: width || '100%' } }>
-      { isLoad && <Loader/> }
-
-      { errorMessage && !isLoad ? (
-        <span className="error-message"> { errorMessage } </span>
-      ) : (
-        <div className="markdown-body" dangerouslySetInnerHTML={ { __html: markdown } }/>
-      ) }
+      <MdComponent isLoading={ isLoading } errorMessage={ errorMessage } markdown={ markdown }/>
     </div>
   );
 }
 
+function MdComponent({ isLoading, errorMessage, markdown }) {
+  if (isLoading) {
+    return <Loader/>;
+  }
+
+  if (errorMessage) {
+    return <span className="error-message"> { errorMessage } </span>;
+  }
+
+  return <div className="markdown-body" dangerouslySetInnerHTML={ { __html: markdown } }/>;
+}
+
 function Loader() {
   return (
-    <div class="lds-ring">
+    <div className="lds-ring">
       <div></div>
       <div></div>
       <div></div>
