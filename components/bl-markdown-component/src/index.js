cbf6e83b384bac317d3da0061917d6c00f1bb909
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-10-26 13:12:14 +0300

    deleted loadMd function and refactored code style

--- components/bl-markdown-component/src/index.js
+++ components/bl-markdown-component/src/index.js
@@ -4,6 +4,8 @@ import MarkdownIt from './markdown-it.min.js';
 
 const { cn } = BackendlessUI.CSSUtils;
 
+const NO_MARKDOWN = '<h1>No specified markdown text</h1>';
+
 export default function Markdown({ component }) {
   const { classList, style, display, url, markdownText, height, width } = component;
 
@@ -13,44 +15,24 @@ export default function Markdown({ component }) {
 
   const md = useMemo(() => new MarkdownIt(), []);
 
-  component.setUrl = (url) => {
-    fetchContent(url);
-  };
+  component.setUrl = url => fetchContent(url);
+  component.setContent = text => setContent(text);
 
-  component.setContent = (text) => {
-    setContent(text);
-  };
+  useEffect(() => fetchContent(url), [url]);
+  useEffect(() => setContent(markdownText), [markdownText]);
 
-  useEffect(() => {
-    loadMd(url, markdownText);
-  }, [markdownText, url]);
-
-  const fetchContent = (url) => {
+  const fetchContent = url => {
     setIsLoading(true);
+    setErrorMessage('');
+
     fetch(url)
       .then(response => response.text())
-      .then((data) => {
-        setContent(data);
-      })
-      .catch((error) => {
-        setErrorMessage(error.message);
-      })
+      .then(data => setContent(data))
+      .catch(error => setErrorMessage(error.message))
       .finally(() => setIsLoading(false));
   };
 
-  const setContent = (text) => {
-    setMarkdown(md.render(text));
-  };
-
-  const loadMd = (url, text) => {
-    if (text) {
-      setContent(text);
-    } else if (url) {
-      fetchContent(url);
-    } else {
-      setMarkdown('<h1>No specified markdown text</h1>');
-    }
-  };
+  const setContent = text => setMarkdown(md.render(text));
 
   if (!display) {
     return null;
@@ -60,7 +42,7 @@ export default function Markdown({ component }) {
     <div
       className={ cn('bl-customComponent-markdown markdown-body', classList) }
       style={ { ...style, height: height || '100%', width: width || '100%' } }>
-      <MdContent isLoading={ isLoading } errorMessage={ errorMessage } markdown={ markdown }/>
+      <MdContent isLoading={ isLoading } errorMessage={ errorMessage } markdown={ markdown || NO_MARKDOWN }/>
     </div>
   );
 }
