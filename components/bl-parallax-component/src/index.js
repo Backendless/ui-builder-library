3eea947601bb786ca07ffad8d6b92653018beba4
Pavlo Stasiv <99363993+StasivPavlo@users.noreply.github.com>
2022-10-26 12:50:14 +0300

    refactored component for pod, implemented logic without lib, changed README.md

--- components/bl-parallax-component/src/index.js
+++ components/bl-parallax-component/src/index.js
@@ -1,29 +1,49 @@
-import { Parallax } from './react-parallax.min';
+import { useRef, useEffect, useCallback } from 'react';
 
 const { cn } = BackendlessUI.CSSUtils;
 
-export default function ParallaxComponent({ component }) {
-  const { style, display, classList, image, strength, title, text } = component;
+export default function Parallax({ component, pods }) {
+  const { display, style, classList, image, strength } = component;
+  const parallaxContentPod = pods['parallaxContent'];
+
+  const backgroundImageElement = useRef();
+  const containerElement = useRef();
+
+  const parallax = useCallback((containerElementCenterOffset) => {
+    const backgroundHeight = backgroundImageElement.current.getBoundingClientRect().height;
+    const screenCenter = window.innerWidth / 2;
+    const distanceToCenterOfScreen = containerElementCenterOffset - screenCenter - window.pageYOffset;
+    const translateY = (distanceToCenterOfScreen / backgroundHeight * -100) * strength * 0.001;
+
+    backgroundImageElement.current.style.transform = `translate(0, calc(${ translateY }% - ${ strength / 2 }px))`;
+  }, []);
+
+  useEffect(() => {
+    backgroundImageElement.current.style.height = `calc(100% + ${ strength }px)`;
+
+    const containerElementTop = containerElement.current.getBoundingClientRect().top;
+    const containerElementCenter = containerElement.current.getBoundingClientRect().height / 2;
+    const containerElementCenterOffset = containerElementTop + containerElementCenter;
+
+    parallax(containerElementCenterOffset);
+    document.addEventListener('scroll', () => parallax(containerElementCenterOffset));
+
+    return () => {
+      document.removeEventListener('scroll', parallax);
+    };
+  }, []);
 
   if (!display) {
     return null;
   }
 
   return (
-    <Parallax
-      className={ cn('bl-customComponent-parallax', 'parallax', classList) }
-      style={ style }
-      bgImage={ image }
-      strength={ strength }
-      contentClassName="parallax__content"
-      bgClassName="parallax__image">
-      { title && (
-        <h3 className="parallax__title">{ title }</h3>
-      ) }
-
-      { text && (
-        <p className="parallax__text">{ text }</p>
-      ) }
-    </Parallax>
+    <div ref={ containerElement } className={ cn('bl-customComponent-parallax', classList) } style={ style }>
+      <div ref={ backgroundImageElement } className="parallax-background-img"
+           style={ { backgroundImage: `url(${ image })` } }></div>
+      <div className="parallax-content">
+        { parallaxContentPod.render() }
+      </div>
+    </div>
   );
 }
