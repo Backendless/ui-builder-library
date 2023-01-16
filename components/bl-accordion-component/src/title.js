dd9caadce838df0d9e667ca7fd15045f7550a9c9
serhmel <93279075+serhmel@users.noreply.github.com>
2023-01-16 16:12:39 +0200

    MARKET-1591 Accordion (#193)
    * fix text label shift right away after page refreshing
    
    * move collapse icon to the separate component and add a reference to the root element
    
    * refactoring

--- components/bl-accordion-component/src/title.js
+++ components/bl-accordion-component/src/title.js
@@ -19,8 +19,16 @@ export function Title({ options }) {
       aria-controls={ `item-${ accordionId }-${ index }-content` }
       role="button"
       tabIndex="0">
-      <i className="material-icons-round" aria-hidden="true">keyboard_arrow_right</i>
+      <CollapseIcon/>
       { title }
     </div>
+  );
+}
+
+function CollapseIcon() {
+  return (
+    <svg className="collapse-icon" focusable="false" aria-hidden="true" viewBox="0 0 24 24">
+      <path d="M6.23 20.23 8 22l10-10L8 2 6.23 3.77 14.46 12z"></path>
+    </svg>
   );
 }
