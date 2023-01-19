636541786396bcda2d71de9404f01cd3460efe02
Fialkaksu <fialkaksu1984@gmail.com>
2023-01-19 15:27:19 +0200

    code edits

--- components/bl-totop-component/src/index.js
+++ components/bl-totop-component/src/index.js
@@ -1,53 +1,56 @@
-const { cn } = BackendlessUI.CSSUtils
+const { cn } = BackendlessUI.CSSUtils;
+
+const Transform = {
+  MAIN: "50%",
+  X: "translateX(-50%)",
+  Y: "translateY(-50%)",
+};
 
 export default function TotopComponent({ component }) {
-  const { style, classList, offset, position, backgroundColor, color, size, padding, indent, element } = component
+  const {
+    classList,
+    offset,
+    position,
+    backgroundColor,
+    color,
+    size,
+    iconSize,
+    indent = component.indent ? component.indent : "20px",
+    element,
+  } = component;
 
-  style.backgroundColor = backgroundColor
-  style.padding = padding
-  style.bottom = indent
-
-  if (position === 'bottom-right') {
-    style.right = indent
-  } else {
-    style.left = indent
-  }
-
-  const iconStyles = {
+  const styles = {
+    backgroundColor,
     width: size,
     height: size,
-    fill: color,
-  }
+    top: position.includes("top") ? indent : position.includes("center-") ? Transform.MAIN : undefined,
+    bottom: position.includes("bottom") ? indent : position.includes("center-") ? Transform.MAIN : undefined,
+    right: position.includes("right") ? indent : position.includes("-center") ? Transform.MAIN : undefined,
+    left: position.includes("left") ? indent : position.includes("-center") ? Transform.MAIN : undefined,
+    transform: position.includes("-center") ? Transform.X : position.includes("center-") ? Transform.Y : undefined,
+  };
 
-  const handleScroll = () => {
-    if (element && element.el) {
-      window.scrollTo({
-        top: element.el.offsetTop + offset,
-        behavior: 'smooth',
-      })
-    } else {
-      window.scrollTo({
-        top: offset,
-        behavior: 'smooth',
-      })
-    }
-  }
+  const iconStyles = {
+    width: iconSize,
+    height: iconSize,
+    fill: color,
+  };
+
+  const handleScroll = () => window.scrollTo({ top: element?.el ? offset : offset, behavior: "smooth" });
 
   return (
-    <div className={ cn('bl-customComponent-totop', classList) } style={ style } onClick={ handleScroll }>
-      <TotopIcon iconStyles={ iconStyles } />
+    <div className={cn("bl-customComponent-totop", classList)} style={styles} onClick={handleScroll}>
+      <TotopIcon iconStyles={iconStyles} />
     </div>
-  )
+  );
 }
 
 function TotopIcon({ iconStyles }) {
   return (
-    <div className="totop__icon" style={ iconStyles }>
+    <div className="totop__icon" style={iconStyles}>
       <svg viewBox="0 0 330 330">
-      <path
-          d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z"
-        />
+        <path d="M325.606,229.393l-150.004-150C172.79,76.58,168.974,75,164.996,75c-3.979,0-7.794,1.581-10.607,4.394l-149.996,150c-5.858,5.858-5.858,15.355,0,21.213c5.857,5.857,15.355,5.858,21.213,0l139.39-139.393l139.397,139.393C307.322,253.536,311.161,255,315,255c3.839,0,7.678-1.464,10.607-4.394C331.464,244.748,331.464,235.251,325.606,229.393z" />
       </svg>
     </div>
-  )
+  );
 }
