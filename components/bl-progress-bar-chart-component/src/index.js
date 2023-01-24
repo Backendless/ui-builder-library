ce2667aa82fb15e381c4ea99cbfd43d63b9211ab
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-01-24 20:39:40 +0200

    refactoring, add root ref to component.el

--- components/bl-progress-bar-chart-component/src/index.js
+++ components/bl-progress-bar-chart-component/src/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useMemo } from 'react';
+import { useState, useEffect, useMemo, useRef } from 'react';
 
 import { Bar } from './bar';
 
@@ -12,13 +12,13 @@ export default function ProgressBarChartComponent({ component }) {
     classList, style, display, disabled, visibility, chartOrientation, gridMarks, height, width, data
   } = component;
 
+  const rootRef = useRef();
   const [chartData, setChartData] = useState([]);
   const marksList = useGridMarks(gridMarks);
   const isHorizontal = useMemo(() => chartOrientation === HORIZONTAL, [chartOrientation]);
 
-  useEffect(() => {
-    setChartData(validate(data));
-  }, [data]);
+  useEffect(() => { setChartData(validate(data)); }, [data]);
+  useEffect(() => { component.el = rootRef.current; }, [rootRef]);
 
   const classes = cn(
     'bl-customComponent-progressBarChart', classList,
@@ -33,19 +33,11 @@ export default function ProgressBarChartComponent({ component }) {
   }
 
   return (
-    <div
-      style={ styles }
-      className={ classes }>
+    <div ref={ rootRef } style={ styles } className={ classes }>
       <div className="chart">
         <div className="chart__items">
-          { visibility &&
-            <div className="chart__grids">
-              { marksList.map(item => (
-                <div className="chart__y-grid-line" style={{ [isHorizontal ? 'left' : 'bottom']: `${ item }%` }} />
-              )) }
-            </div>
-          }
-          { chartData.length > 0 && chartData.map(({ id, name, goal, progress }) => (
+          { visibility && <Grids marksList={ marksList } isHorizontal={ isHorizontal } />}
+          { chartData.map(({ id, name, goal, progress }) => (
             <Bar
               key={ id }
               name={ name }
@@ -56,18 +48,32 @@ export default function ProgressBarChartComponent({ component }) {
             />
           )) }
         </div>
-        { visibility &&
-          <div className="chart__percentage-marks">
-            { marksList.map(item => (
-              <div
-                className="chart__percentage-marks-item"
-                style={{ [isHorizontal ? 'left' : 'bottom']: `${ item }%`}}>
-                { `${ item }%` }
-              </div>
-            )) }
-          </div>
-        }
+        { visibility && <PercentageMarks marksList={ marksList } isHorizontal={ isHorizontal } /> }
       </div>
+    </div>
+  );
+}
+
+function Grids({ marksList, isHorizontal }) {
+  return (
+    <div className="chart__grids">
+      { marksList.map(item => (
+        <div className="chart__y-grid-line" style={{ [isHorizontal ? 'left' : 'bottom']: `${ item }%` }} />
+      )) }
+    </div>
+  );
+}
+
+function PercentageMarks({ marksList, isHorizontal }) {
+  return (
+    <div className="chart__percentage-marks">
+      { marksList.map(item => (
+        <div
+          className="chart__percentage-marks-item"
+          style={{ [isHorizontal ? 'left' : 'bottom']: `${ item }%`}}>
+          { `${ item }%` }
+        </div>
+      )) }
     </div>
   );
 }
