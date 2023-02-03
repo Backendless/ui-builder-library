7a2c02bf4d0fa3ea393fb325f5cee6ed476c5db7
Zahar Mychka <56361878+Saharij@users.noreply.github.com>
2023-02-03 13:51:06 +0200

    BKNDLSS-31144 Purpose of "value" select in settings (#243)
    * fix Value handler, describe the purpose of Value
    
    * minor fix
    
    * fix component ref adding
    
    ---------
    
    Co-authored-by: Zakhar Mychka <zakharii.mychka@plus4u.net>

--- components/bl-multiple-select-component/src/helpers/validate.js
+++ components/bl-multiple-select-component/src/helpers/validate.js
@@ -1,9 +1,13 @@
-export const validate = items => {
-  if (!Array.isArray(items)) {
-    console.error("Options should be a list");
+export const validateOptions = options => {
+  if (!Array.isArray(options)) {
+    console.error('Options should be a list');
 
     return [];
   }
 
-  return items;
+  return options;
+};
+
+export const validateValue = (value, options) => {
+  return options.filter(item => value.includes(item.value));
 };
