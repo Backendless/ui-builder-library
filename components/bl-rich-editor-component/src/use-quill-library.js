74b99fe026581680151f9c412d5bfe35a8f6af9c
Sergiy Melnychuk <serh.mel@gmail.com>
2023-02-10 19:41:36 +0200

    add inline styles to the editor content

--- components/bl-rich-editor-component/src/use-quill-library.js
+++ components/bl-rich-editor-component/src/use-quill-library.js
@@ -2,17 +2,84 @@ import { useEffect, useRef } from 'react';
 
 import Quill from './lib/quil.min';
 
+const FontSize = [
+  '8px', '9px', '10px', '11px', '12px', '14px', '16px', '18px', '20px', '22px', '24px', '26px', '28px', '36px', '48px',
+];
+
+const FontFamily = [
+  'arial, helvetica, sans-serif', 'Comic Sans MS, cursive, sans-serif', 'courier new, courier, monospace',
+  'georgia, palatino, serif', 'helvetica, arial, sans-serif', 'Lucida Sans Unicode, Lucida Grande, sans-serif',
+  'andale mono, monospace', 'book antiqua, palatino, serif', 'impact, sans-serif',
+  'tahoma, arial, helvetica, sans-serif', 'terminal, monaco, monospace', 'times new roman, times, serif',
+  'trebuchet ms, geneva, sans-serif', 'verdana, geneva, sans-serif',
+];
+
+export const DefaultStyles = {
+  fontSize  : '14px',
+  fontFamily: 'arial, helvetica, sans-serif',
+};
+
+const BlockquoteStyles = {
+  borderLeft  : '4px solid #ccc',
+  marginBottom: '5px',
+  marginTop   : '5px',
+  paddingLeft : '16px',
+};
+
+const CodeStyles = {
+  whiteSpace  : 'pre-wrap',
+  marginBottom: '5px',
+  marginTop   : '5px',
+  padding     : '5px 10px',
+  borderRadius: '3px',
+  background  : '#808080',
+  color       : '#ffffff',
+};
+
 export const Size = Quill.import('formats/size');
 export const Font = Quill.import('formats/font');
+const AlignStyle = Quill.import('attributors/style/align');
+const DirectionStyle = Quill.import('attributors/style/direction');
+const FontStyle = Quill.import('attributors/style/font');
+const SizeStyle = Quill.import('attributors/style/size');
+const Blockquote = Quill.import('formats/blockquote');
+const CodeBlock = Quill.import('formats/code-block');
+const BlockPrototype = Quill.import('blots/block');
 
-Size.whitelist = ['8', '9', '10', '11', '12', '14', '16', '18', '20', '22', '24', '26', '28', '36', '48'];
-Font.whitelist = [
-  'arial', 'comic-sans', 'courier-new', 'georgia', 'helvetica', 'lucida', 'andale-mono',
-  'book-antiqua', 'impact', 'tahoma', 'terminal', 'times-new-roman', 'trebuchet-ms', 'verdana',
-];
+Size.whitelist = FontSize;
+Font.whitelist = FontFamily;
+SizeStyle.whitelist = FontSize;
+FontStyle.whitelist = FontFamily;
+
+addInlineStyles(Blockquote, BlockquoteStyles);
+addInlineStyles(CodeBlock, CodeStyles);
+
+class Block extends BlockPrototype {
+  constructor(domNode, value) {
+    super(domNode, value);
+
+    this.format('fontSize', DefaultStyles.fontSize);
+    this.format('fontFamily', DefaultStyles.fontFamily);
+  }
+
+  format(name, value) {
+    if (DefaultStyles[name]) {
+      this.domNode.style[name] = value;
+    } else {
+      super.format(name, value);
+    }
+  }
+}
 
 Quill.register(Size, true);
 Quill.register(Font, true);
+Quill.register(AlignStyle, true);
+Quill.register(DirectionStyle, true);
+Quill.register(FontStyle, true);
+Quill.register(SizeStyle, true);
+Quill.register(Blockquote, true);
+Quill.register(CodeBlock, true);
+Quill.register(Block, true);
 
 export function useQuillLibrary(quillRef, toolbarRef, component, onTextChange) {
   const { placeholder, readOnly, content } = component;
@@ -57,4 +124,16 @@ export function useQuillLibrary(quillRef, toolbarRef, component, onTextChange) {
   const redo = () => editorRef.current.history.redo();
 
   return editorRef;
+}
+
+function addInlineStyles(block, styles) {
+  const createBlock = block.create;
+
+  block.create = function() {
+    const node = createBlock.call(this);
+
+    Object.keys(styles).forEach(key => node.style[key] = styles[key]);
+
+    return node;
+  };
 }
