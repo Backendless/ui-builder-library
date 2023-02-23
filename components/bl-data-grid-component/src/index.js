e104b5635b9b6131ebe9629b8690100ba31c6964
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-02-23 20:12:00 +0200

    refactoring

--- components/bl-data-grid-component/src/index.js
+++ components/bl-data-grid-component/src/index.js
@@ -1,13 +1,10 @@
 import { useState, useEffect, useRef, useMemo, useCallback, memo } from 'react';
+
+import { EmptyMessage } from './empty-message';
+
 import { AgGridReact } from './lib/ag-grid-react.min.js';
 
 const { cn } = BackendlessUI.CSSUtils;
-
-const messages = {
-  ROWS: 'Rows Are Empty',
-  COLUMNS: 'Columns Are Empty',
-  ROWSANDCOLUMNS: 'Columns And Rows Are Empty'
-};
 
 function CellComponent(params) {
   return (<span>{ params.value }</span>);
@@ -24,8 +21,8 @@ export default function DataGridComponent({ component, eventHandlers }) {
   const [rows, setRows] = useState([]);
 
   useEffect(() => {
-    setColumns(() => (columnDefs ? columnDefs : []));
-    setRows(() => (rowData ? rowData : []));
+    setColumns(() => (columnDefs || []));
+    setRows(() => (rowData || []));
   }, [columnDefs, rowData]);
 
   const defaultColDef = useMemo(() => ({
@@ -41,7 +38,6 @@ export default function DataGridComponent({ component, eventHandlers }) {
   }, []);
 
   const styles = { ...style, height: `${ height }px`, width: `${ width }px`, flexShrink: 0 };
-  const message = getMessage(columns, rows);
   const classes = cn(
     'bl-customComponent-dataGrid', `ag-theme-${ theme }`, classList,
     { 'bl-customComponent-dataGrid--disabled': disabled }
@@ -51,48 +47,20 @@ export default function DataGridComponent({ component, eventHandlers }) {
     return null;
   }
 
-  if (!columns.length || !rows.length) {
-    return (
-      <div style={ styles } className={ classes }>
-        <div className="ag-root-wrapper ag-overlay-panel">
-          <div className="ag-overlay-wrapper">
-            <div className="ag-overlay-background" />
-            <div className="ag-overlay-no-rows-wrapper">
-              <span className="ag-overlay-no-rows-center">{ message }</span>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div
       style={ styles }
       className={ classes }>
-      <AgGridReact
-        ref={ gridRef }
-        rowData={ rows }
-        columnDefs={ columns }
-        defaultColDef={ defaultColDef }
-        onCellClicked={ handleCellClick }
-      />
+      { !columns.length || !rows.length
+        ? <EmptyMessage noColumns={ !columns.length } noRows={ !rows.length } />
+        : <AgGridReact
+            ref={ gridRef }
+            rowData={ rows }
+            columnDefs={ columns }
+            defaultColDef={ defaultColDef }
+            onCellClicked={ handleCellClick }
+          />
+      }
     </div>
   );
 }
-
-const getMessage = (columns, rows) => {
-  if (!columns.length && !rows.length) {
-    return messages.ROWSANDCOLUMNS;
-  }
-
-  if (!columns.length) {
-    return messages.COLUMNS;
-  }
-
-  if (!rows.length) {
-    return messages.ROWS;
-  }
-
-  return '';
-};
