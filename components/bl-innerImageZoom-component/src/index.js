029d306fa9747f18cba8d02242553d79ed2366f0
Oksana Dmytriieva <46996947+Fialkaksu@users.noreply.github.com>
2023-03-14 10:36:43 +0200

    MARKET-1644 Inner Image Zoom (#293)
    * added component
    
    * code edits
    
    * minor edits
    
    * refactoring
    
    * refactoring
    
    * reorder of lines of code
    
    * reorder of lines of code

---
+++ components/bl-innerImageZoom-component/src/index.js
@@ -0,0 +1,49 @@
+import { useMemo, useState } from 'react';
+
+const { cn } = BackendlessUI.CSSUtils;
+const DEFAULT_ZOOM_POSITION = '0% 0%';
+
+export default function InnerImageZoomComponent({ component, elRef, eventHandlers }) {
+  const { classList, style, display, source, zoomIn } = component;
+  const { onMouseOver, onMouseOut, onMouseMove } = eventHandlers;
+
+  const [zoomPosition, setZoomPosition] = useState(DEFAULT_ZOOM_POSITION);
+
+  const zoom = Math.max(100, zoomIn);
+
+  const figureStyle = useStyles(source, zoomPosition, zoom);
+
+  const handleMouseMove = event => {
+    const { left, top, width, height } = event.target.getBoundingClientRect();
+    const x = (event.pageX - left - window.scrollX) / width * 100;
+    const y = (event.pageY - top - window.scrollY) / height * 100;
+
+    setZoomPosition(`${ x }% ${ y }%`);
+    onMouseMove({ event });
+  };
+
+  if (!display) {
+    return null;
+  }
+
+  return (
+    <div ref={ elRef } className={ cn('bl-customComponent-innerImageZoom', ...classList) } style={ style }>
+      <figure
+        className="zoom-content"
+        style={ figureStyle }
+        onMouseOver={ onMouseOver }
+        onMouseMove={ handleMouseMove }
+        onMouseOut={ onMouseOut }>
+        <img src={ source } className="img" />
+      </figure>
+    </div>
+  );
+}
+
+function useStyles(src, zoomPosition, zoom) {
+  return useMemo(() => ({
+    backgroundImage   : `url(${ src })`,
+    backgroundPosition: zoomPosition,
+    backgroundSize    : `${ zoom }% ${ zoom }%`,
+  }), [src, zoomPosition, zoom]);
+}
