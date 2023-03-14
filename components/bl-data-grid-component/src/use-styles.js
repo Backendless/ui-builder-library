19d9a382947b865c62b8edb77c05f71f920c3b2b
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-03-14 16:05:36 +0200

    minor fix

--- components/bl-data-grid-component/src/use-styles.js
+++ components/bl-data-grid-component/src/use-styles.js
@@ -1,5 +1,7 @@
 import { useMemo } from 'react';
 
+const ONLY_NUMERIC_REGEX = /^\d+$/;
+
 export const useStyles = (style, width, height) => useMemo(() => {
   const validStyles = validateStyles({ width, height });
 
@@ -7,10 +9,8 @@ export const useStyles = (style, width, height) => useMemo(() => {
 }, [style, width, height]);
 
 const validateStyles = dimensions => {
-  const numbersOnlyRegex = /^\d+$/;
-
   return Object.entries(dimensions).reduce((acc, [propertyName, value]) => {
-    if (numbersOnlyRegex.test(value)) {
+    if (ONLY_NUMERIC_REGEX.test(value)) {
       console.warn(`${propertyName} automatically changed (${value} -> ${value}px) as it is an invalid CSS value.`);
 
       return { ...acc, [propertyName]: `${ value }px` };
