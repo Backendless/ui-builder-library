565be36f7c8e5489f0da4af0155a6e30f91805a7
Fialkaksu <fialkaksu1984@gmail.com>
2023-03-17 18:01:58 +0200

    component init

---
+++ components/bl-marquee-component/README.md
@@ -0,0 +1,49 @@
+# Marquee
+
+Marquee is the component that can be used in Backendless [UI-Builder](https://backendless.com/developers/#ui-builder). It allows the user to apply a lightweight React component that utilizes the power of CSS animations to create silky smooth marquees in the application.
+
+<p align="center">
+  <img src="./thumbnail.png" alt="main thumbnail" width="780"/>
+</p>
+
+## Configuration
+
+Configuration can be done in the UI Builder. You have to specify the content that will be displayed in the component and to which the effect will be applied.
+
+## Properties
+
+
+
+## Events
+
+
+
+## Styles
+
+**General**
+
+````
+@bl-customComponent-marquee-overlay-background: linear-gradient(to right, rgba(255, 255, 255, 1), rgba(255, 255, 255, 0));
+@bl-customComponent-marquee-overlay-z-index: 2;
+````
+
+**Dimensions**
+
+````
+@bl-customComponent-marquee-width: 100%;
+@bl-customComponent-marquee-content-width: 100%;
+@bl-customComponent-marquee-overlay-width: 100%;
+@bl-customComponent-marquee-overlay-height: 100%;
+@bl-customComponent-marquee-overlay-pseudo-width: 200px;
+````
+
+**Other**
+
+````
+@bl-customComponent-marquee-animation-name: scroll;
+@bl-customComponent-marquee-animation-timing-function: linear;
+@bl-customComponent-marquee-animation-delay: 0s;
+@bl-customComponent-marquee-animation-direction: normal;
+@bl-customComponent-marquee-animation-duration: 10s;
+@bl-customComponent-marquee-animation-iteration-count: infinite;
+````
---
+++ components/bl-marquee-component/src/index.js
@@ -0,0 +1,100 @@
+import { useCallback, useEffect, useMemo, useRef, useState } from 'react';
+
+const { cn } = BackendlessUI.CSSUtils;
+
+const DEFAULT_COUNT = 'infinite';
+const PlayState = { RUN: 'running', PAUSE: 'paused' };
+
+export default function MarqueeComponent({ component, elRef, eventHandlers, pods }) {
+  const {
+    classList, style, display, start, pauseOnHover, pauseOnClick, direction, speed, delay, loop, gradient
+  } = component;
+  const { onFinish, onCycleComplete } = eventHandlers;
+  const children = pods['marqueeContent'].render();
+
+  const [isMounted, setIsMounted] = useState(false);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [marqueeWidth, setMarqueeWidth] = useState(0);
+  const [hasFinished, setHasFinished] = useState(false);
+  const [play, setPlay] = useState(start);
+  const marqueeRef = useRef(null);
+
+  useEffect(() => {
+    setIsMounted(true);
+  }, []);
+
+  useEffect(() => {
+    if (!isMounted) return;
+
+    const calculateWidth = () => {
+      if (marqueeRef.current && elRef.current) {
+        setContainerWidth(elRef.current.getBoundingClientRect().width);
+        setMarqueeWidth(marqueeRef.current.getBoundingClientRect().width);
+      }
+    };
+
+    calculateWidth();
+    window.addEventListener('resize', calculateWidth);
+
+    return () => {
+      window.removeEventListener('resize', calculateWidth);
+    };
+  }, [isMounted]);
+
+  const duration = useMemo(() => {
+    return marqueeWidth < containerWidth ? containerWidth / speed : marqueeWidth / speed;
+  }, [marqueeWidth, containerWidth, speed]);
+
+  const styleMarquee = useMemo(() => {
+    return {
+      '--marquee-play': play ? PlayState.RUN : PlayState.PAUSE,
+      animationDelay: `${delay}s`,
+      animationDuration: `${duration}s`,
+      animationDirection: direction,
+      animationIterationCount: !!loop ? loop : DEFAULT_COUNT,
+    };
+  }, [play, direction, duration, delay, loop]);
+
+  const styles = useMemo(() => {
+    return {
+      ...style,
+      '--marquee-pause-on-hover': !play || pauseOnHover ? PlayState.PAUSE : PlayState.RUN,
+      '--marquee-pause-on-click':
+        !play || (pauseOnHover && !pauseOnClick) || pauseOnClick ? PlayState.PAUSE : PlayState.RUN,
+    };
+  }, [style, play, pauseOnHover, pauseOnClick]);
+
+  const handleFinish = useCallback(() => {
+    if (!hasFinished) {
+      setHasFinished(true);
+    } else {
+      onFinish();
+    }
+  }, [onFinish, hasFinished]);
+
+  component.startPlay = () => { setPlay(true) };
+  component.stopPlay = () => { setPlay(false) };
+
+  if (!display || !isMounted) {
+    return null;
+  }
+
+  return (
+    <div ref={ elRef } className={ cn('bl-customComponent-marquee', ...classList) } style={ styles }>
+      { gradient && <div className="overlay" /> }
+
+      <div
+        ref={ marqueeRef }
+        style={ styleMarquee }
+        className="marquee-content"
+        onAnimationIteration={ onCycleComplete }
+        onAnimationEnd={ handleFinish }>
+        {children}
+      </div>
+
+      <div style={ styleMarquee } className="marquee-content" aria-hidden="true">
+        { children }
+      </div>
+    </div>
+  );
+}
