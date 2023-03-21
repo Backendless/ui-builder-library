1482110f5d80c127393c651ea0e7050816e98eba
Zahar Mychka <56361878+Saharij@users.noreply.github.com>
2023-03-21 11:04:46 +0200

    MARKET-1733 [Double Calendar] Add date format selection in settings (#396)
    * create normalizeFormat function to set format to the standard
    
    * minor fix
    
    * fix Date Format handler
    
    * refactoring
    
    ---------
    
    Co-authored-by: Zakhar Mychka <zakharii.mychka@plus4u.net>

--- components/bl-double-calendar-component/src/helpers/use-actions.js
+++ components/bl-double-calendar-component/src/helpers/use-actions.js
@@ -1,20 +1,23 @@
-import { differenceInTime } from './utils';
+import { differenceInTime, normalizeFormat } from './utils';
 
-export function useActions({ component, startDate, endDate, daysAmount, setStartDate, setEndDate }) {
+export function useActions({ component, dateFormat, startDate, endDate, daysAmount, setStartDate, setEndDate }) {
   Object.assign(component, {
     getFromDate     : () => startDate,
-    setFromDate     : fromDate => setStartDate(new Date(fromDate)),
+    setFromDate     : fromDate => setStartDate(new Date(normalizeFormat(fromDate, dateFormat))),
     getToDate       : () => endDate,
-    setToDate       : toDate => setEndDate(new Date(toDate)),
+    setToDate       : toDate => setEndDate(new Date(normalizeFormat(toDate, dateFormat))),
     getFromAndToDate: () => ({ fromDate: startDate, toDate: endDate }),
     setFromAndToDate: (fromDate, toDate) => {
-      const diffInTime = differenceInTime(new Date(fromDate), new Date(toDate));
+      const normalizedFromDate = new Date(normalizeFormat(fromDate, dateFormat));
+      const normalizedToDate = new Date(normalizeFormat(toDate, dateFormat));
+
+      const diffInTime = differenceInTime(normalizedFromDate, normalizedToDate);
 
       if (diffInTime >= 0) {
-        setStartDate(new Date(fromDate));
-        setEndDate(new Date(toDate));
+        setStartDate(normalizedFromDate);
+        setEndDate(normalizedToDate);
       } else {
-        console.warn("Date is not valid!");
+        console.warn("The date in the From Date property is later than the To Date! Please pass a valid date.");
       }
     },
     getDaysAmount   : () => daysAmount,
--- components/bl-double-calendar-component/src/helpers/utils.js
+++ components/bl-double-calendar-component/src/helpers/utils.js
@@ -13,3 +13,25 @@ export function differenceInDays(start, end) {
 
   return Math.round(diffInTime / ONE_DAY) + 1;
 }
+
+function getFormatPartsOrder(format) {
+  return format.split("-").reduce((acc, item, index) => {
+    return {
+      ...acc,
+      [item]: index,
+    };
+  }, {});
+}
+
+export function normalizeFormat(date, format) {
+  if (!date) {
+    console.warn('Date is not provided!')
+
+    return null;
+  }
+
+  const dateParts = date.split("-");
+  const { DD, MM, YYYY } = getFormatPartsOrder(format);
+
+  return `${dateParts[MM]}-${dateParts[DD]}-${dateParts[YYYY]}`;
+}
