ca2f3a386cebb442d3130f5822bb2cb51eef7ce9
Fialkaksu <fialkaksu1984@gmail.com>
2023-03-30 17:42:28 +0300

    refactoring

--- components/bl-weather-component/src/index.js
+++ components/bl-weather-component/src/index.js
@@ -9,12 +9,13 @@ const UNITS = {
   metric  : { temperature: '°C', windSpeed: 'm/s' },
   imperial: { temperature: '°F', windSpeed: 'mph' },
 };
+const BASE_URL = 'https://api.openweathermap.org/data/2.5';
 
 export default function WeatherComponent({ component, elRef, settings }) {
   const { classList, style, display, currentLocation, city, coords, forecast, unit, theme, lang } = component;
   const { API_KEY } = settings;
 
-  const { lat, lon, weatherCity, hasGeoAccess, fetchWeatherCity } = useWeather(currentLocation, city, coords, API_KEY);
+  const { location, weatherCity, hasGeoAccess } = useOptions(currentLocation, city, coords, API_KEY);
 
   if (!display) {
     return null;
@@ -22,11 +23,11 @@ export default function WeatherComponent({ component, elRef, settings }) {
 
   return (
     <div ref={ elRef } className={ cn('bl-customComponent-weather', ...classList) } style={ style }>
-      { hasGeoAccess && lat && lon && (
+      { hasGeoAccess && location?.lat && location.lng && (
         <Weather
           settings={ settings }
-          lat={ lat }
-          lon={ lon }
+          lat={ location.lat }
+          lon={ location.lng }
           weatherCity={ weatherCity }
           showForecast={ forecast }
           unit={ unit }
@@ -60,49 +61,49 @@ function Weather({ settings, lat, lon, weatherCity, showForecast, theme, unit, l
   );
 }
 
-function useWeather(currentLocation, city, coords, API_KEY) {
-  const [lat, setLat] = useState(null);
-  const [lon, setLon] = useState(null);
+function useOptions(currentLocation, city, coords, API_KEY) {
+  const [location, setLocation] = useState(null);
   const [weatherCity, setWeatherCity] = useState(null);
   const [hasGeoAccess, setHasGeoAccess] = useState(true);
 
   useEffect(() => {
     if (currentLocation && navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(
-        function(position) {
-          setLat(position.coords.latitude);
-          setLon(position.coords.longitude);
-        },
-        function(error) {
-          console.log(error);
+        position => setLocation({
+          lat: position.coords.latitude,
+          lng: position.coords.longitude }),
+        error => {
+          console.error(error);
           setHasGeoAccess(false);
         }
       );
     } else if (city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${ city }&appid=${ API_KEY }&units=metric`)
+      fetch(`${ BASE_URL }/weather?q=${ city }&appid=${ API_KEY }`)
         .then(response => response.json())
         .then(data => {
-          setLat(data.coord.lat);
-          setLon(data.coord.lon);
+          setLocation({
+            lat: data.coord.lat,
+            lng: data.coord.lon });
           setWeatherCity(data.name);
         });
     } else if (coords?.lat && coords.lng) {
-      setLat(coords.lat);
-      setLon(coords.lng);
+      setLocation({
+        lat: coords.lat,
+        lng: coords.lng });
     }
   }, [currentLocation, city, coords, API_KEY]);
 
   useEffect(() => {
-    if (lat && lon && !weatherCity) {
-      fetchWeatherCity(lat, lon);
+    if (location?.lat && location.lng && !weatherCity) {
+      setCityByLocation(location);
     }
-  }, [lat, lon]);
+  }, [location, weatherCity]);
 
-  const fetchWeatherCity = (lat, lon) => {
-    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${ lat }&lon=${ lon }&appid=${ API_KEY }&units=metric`)
+  const setCityByLocation = (location) => {
+    fetch(`${ BASE_URL }/weather?lat=${ location.lat }&lon=${ location.lng }&appid=${ API_KEY }`)
       .then(response => response.json())
       .then(data => setWeatherCity(data.name));
   };
 
-  return { lat, lon, weatherCity, hasGeoAccess, fetchWeatherCity };
+  return { location, weatherCity, hasGeoAccess };
 }
