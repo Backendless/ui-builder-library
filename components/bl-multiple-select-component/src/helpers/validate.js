7a526dfc82155e4b66189d83b95ddaaef1835310
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-03-31 19:15:25 +0300

    replace the error message with the message that Stas suggests

--- components/bl-multiple-select-component/src/helpers/validate.js
+++ components/bl-multiple-select-component/src/helpers/validate.js
@@ -1,6 +1,6 @@
 export const validateOptions = options => {
   if (!Array.isArray(options)) {
-    console.error('Invalid options or options are not provided! Please provide valid options.');
+    console.error('Options should be a list OR you have an empty “Options Logic” handler in Logic, delete or fill it.');
 
     return [];
   }
