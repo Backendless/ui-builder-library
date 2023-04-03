629e1f9b686b8377f6cea820bbfb9400f332d055
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-04-03 23:03:22 +0300

    replace the error message with the message that Vova suggests

--- components/bl-multiple-select-component/src/helpers/validate.js
+++ components/bl-multiple-select-component/src/helpers/validate.js
@@ -1,6 +1,6 @@
 export const validateOptions = options => {
   if (!Array.isArray(options)) {
-    console.error('Options should be a list. Check the Options Logic/Settings.');
+    console.error(`The "Multiple Select" component expects a list of options but it gets ${typeof options} type. Make sure you passed the correct one in Logic or the component Settings`);
 
     return [];
   }
