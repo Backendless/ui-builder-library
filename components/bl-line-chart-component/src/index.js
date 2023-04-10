8e7274f7b953ec7d89628ce0ff8c100e96850536
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-04-11 00:07:30 +0300

    add a few handlers, change default data

--- components/bl-line-chart-component/src/index.js
+++ components/bl-line-chart-component/src/index.js
@@ -5,11 +5,27 @@ import Chart from './chartjs';
 const { cn } = BackendlessUI.CSSUtils;
 
 export default function LineChartComponent({ component, elRef }) {
-  const { classList, display, style, disabled, height, width, type, labels, datasets, options } = component;
+  const {
+    classList, display, style, disabled, height, width, type,
+    chartTitleVisibility, chartTitle, chartTitleFontSize, chartBackgroundColor,
+    yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth, labels, datasets, options
+  } = component;
 
   const chartRef = useRef(null);
 
-  useChart(chartRef, { type, labels, datasets, options });
+  useChart(chartRef, {
+    type,
+    labels,
+    datasets,
+    options,
+    chartTitle,
+    chartTitleFontSize,
+    chartTitleVisibility,
+    yGridLineVisibility,
+    xGridLineVisibility,
+    gridLinesColor,
+    gridLinesWidth
+  });
 
   const styles = { ...style, width, height };
 
@@ -24,16 +40,51 @@ export default function LineChartComponent({ component, elRef }) {
       className={
         cn("bl-customComponent-lineChart", classList, { "bl-customComponent-lineChart--disabled": disabled })
       }>
-      <canvas ref={ chartRef } style={{ width: "100%", height: "100%" }} />
+      <canvas ref={ chartRef } style={{ backgroundColor: chartBackgroundColor, width: "100%", height: "100%" }} />
     </div>
   );
 }
 
-const useChart = (chartRef, { type, labels, datasets, options }) => {
+const useChart = (chartRef, props) => {
+  const {
+    type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
+    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
+  } = props;
   const chartInstance = useRef();
 
   useEffect(() => {
-    chartInstance.current = new Chart(chartRef.current, { type, data: { labels, datasets }, options });
+    chartInstance.current = new Chart(chartRef.current, {
+      type,
+      data: { labels, datasets },
+      options: {
+        ...options,
+        plugins: {
+          title: {
+            display: chartTitleVisibility,
+            text: chartTitle,
+            font: {
+              size: chartTitleFontSize
+            }
+          }
+        },
+        scales: {
+          y: {
+            grid: {
+              display: yGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          },
+          x: {
+            grid: {
+              display: xGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            },
+          }
+        }
+      }
+    });
 
     return () => { chartInstance.current.destroy() };
   }, []);
@@ -43,11 +94,41 @@ const useChart = (chartRef, { type, labels, datasets, options }) => {
 
     if (chart) {
       chart.type = type;
-      chart.options = options;
+      chart.options = {
+        ...options,
+        plugins: {
+          title: {
+            display: chartTitleVisibility,
+            text: chartTitle,
+            font: {
+              size: chartTitleFontSize
+            }
+          }
+        },
+        scales: {
+          y: {
+            grid: {
+              display: yGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          },
+          x: {
+            grid: {
+              display: xGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          }
+        }
+      };
       chart.data.labels = labels;
       chart.data.datasets = datasets;
 
       chart.update();
     }
-  }, [type, labels, datasets, options]);
+  }, [
+    type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
+    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
+  ]);
 };
