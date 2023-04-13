23131f5d621327ae07faa9f624a903da8d9aa913
Zahar Mychka <56361878+Saharij@users.noreply.github.com>
2023-04-13 12:05:24 +0300

    MARKET-1626 [Marketplace] Line Chart (#286)
    * MARKET-1626 [Marketplace] Line Chart
    
    * fix link to Codeless Example
    
    * add a few handlers, change default data
    
    * refactoring, add xml code to Codeless examples
    
    ---------
    
    Co-authored-by: Zakhar Mychka <zakharii.mychka@plus4u.net>

---
+++ components/bl-line-chart-component/src/index.js
@@ -0,0 +1,142 @@
+import { useEffect, useRef } from 'react';
+
+import Chart from './chartjs';
+
+const { cn } = BackendlessUI.CSSUtils;
+
+export default function LineChartComponent({ component, elRef }) {
+  const {
+    classList, display, style, disabled, height, width, type,
+    chartTitleVisibility, chartTitle, chartTitleFontSize, chartBackgroundColor,
+    yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth, labels, datasets, options
+  } = component;
+
+  const chartRef = useRef(null);
+
+  useChart(chartRef, {
+    display,
+    type,
+    labels,
+    datasets,
+    options,
+    chartTitle,
+    chartTitleFontSize,
+    chartTitleVisibility,
+    yGridLineVisibility,
+    xGridLineVisibility,
+    gridLinesColor,
+    gridLinesWidth
+  });
+
+  const styles = { ...style, width, height };
+
+  if (!display) {
+    return null;
+  }
+
+  return (
+    <div
+      ref={ elRef }
+      style={ styles }
+      className={
+        cn("bl-customComponent-lineChart", classList, { "bl-customComponent-lineChart--disabled": disabled })
+      }>
+      <canvas ref={ chartRef } style={{ backgroundColor: chartBackgroundColor, width: "100%", height: "100%" }} />
+    </div>
+  );
+}
+
+const useChart = (chartRef, props) => {
+  const {
+    display, type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
+    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
+  } = props;
+  const chartInstance = useRef();
+
+  useEffect(() => {
+    if (!display) {
+      return null;
+    }
+
+    chartInstance.current = new Chart(chartRef.current, {
+      type,
+      data: { labels, datasets },
+      options: {
+        ...options,
+        plugins: {
+          title: {
+            display: chartTitleVisibility,
+            text: chartTitle,
+            font: {
+              size: chartTitleFontSize
+            }
+          }
+        },
+        scales: {
+          y: {
+            grid: {
+              display: yGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          },
+          x: {
+            grid: {
+              display: xGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            },
+          }
+        }
+      }
+    });
+
+    return () => chartInstance.current.destroy();
+  }, [display]);
+
+  useEffect(() => {
+    if (!display) {
+      return null;
+    }
+
+    const { current: chart } = chartInstance;
+
+    if (chart) {
+      chart.type = type;
+      chart.options = {
+        ...options,
+        plugins: {
+          title: {
+            display: chartTitleVisibility,
+            text: chartTitle,
+            font: {
+              size: chartTitleFontSize
+            }
+          }
+        },
+        scales: {
+          y: {
+            grid: {
+              display: yGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          },
+          x: {
+            grid: {
+              display: xGridLineVisibility,
+              color: gridLinesColor,
+              lineWidth: gridLinesWidth
+            }
+          }
+        }
+      };
+      Object.assign(chart.data, { labels, datasets });
+
+      chart.update();
+    }
+  }, [
+    type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
+    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
+  ]);
+};
