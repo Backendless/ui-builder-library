1713e8925421f808996e30af40f6b5802ba221ad
Vladimir Upirov <4648606+Valodya@users.noreply.github.com>
2023-05-03 11:44:45 +0300

    - add template for building extarnal modules

---
+++ components/library-composer/webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+
+const DIST = path.resolve(__dirname, './dist')
+const SRC = path.resolve(__dirname, './src')
+
+module.exports = {
+  entry: path.resolve(SRC, 'index.js'),
+
+  output: {
+    libraryExport: 'default',
+    libraryTarget: 'umd',
+    filename     : 'lib.js',
+    path         : DIST,
+    clean        : true,
+  },
+
+  externals: {
+    react           : 'global React',
+    'react-dom'     : 'global ReactDOM',
+    backendless     : 'global Backendless',
+    'backendless-ui': 'global BackendlessUI',
+  },
+}
