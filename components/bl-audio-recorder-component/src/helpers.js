b6056b6eca37ceb6f5db2bcfca137de0dfd79d47
Roman Skrypak <skrypak1997@gmail.com>
2023-05-11 17:10:27 +0300

    MARKET-1635 Audio Recorder  (#486)
    * MARKET-1635 Add Audio Recorder component
    
    * add width property
    
    * update actions and event, fix issues
    
    * add handlers for control button content, fix issues, update styles
    
    * updated StreamState assigning
    
    * update preview and thumbnail
    
    * updated on State Change

---
+++ components/bl-audio-recorder-component/src/helpers.js
@@ -0,0 +1,23 @@
+export const download = (blob, fileName, fileType) => {
+  const link = document.createElement('a');
+
+  link.style.display = 'none';
+  link.href = URL.createObjectURL(blob);
+  link.download = `${ fileName }.${ fileType }`;
+
+  document.body.appendChild(link);
+  link.click();
+
+  setTimeout(() => {
+    URL.revokeObjectURL(link.href);
+    link.parentNode.removeChild(link);
+  }, 0);
+};
+
+export const captureMediaDevices = async mediaConstraints => {
+  try {
+    return await navigator.mediaDevices.getUserMedia(mediaConstraints);
+  } catch (e) {
+    console.error('The source of the stream did not select.', e);
+  }
+};
