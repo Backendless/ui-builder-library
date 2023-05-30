5ae391bfdccdba1688b4ab64c39d5f5451990edc
Fialkaksu <fialkaksu1984@gmail.com>
2023-05-30 11:33:52 +0300

    refactored

--- components/bl-animatedBox-component/src/index.js
+++ components/bl-animatedBox-component/src/index.js
@@ -3,56 +3,26 @@ import { useCallback, useEffect, useMemo, useState } from 'react';
 const { cn } = BackendlessUI.CSSUtils;
 
 const INFINITE = 'infinite';
-const DEFAULT_ZERO = 0;
-const DEFAULT_ONE = '1s';
+const ONE_SECOND = '1s';
 
 export default function AnimatedBoxComponent({ component, elRef, eventHandlers, pods }) {
-  const { style, classList, display, type, duration, delay, loop, autoStart } = component;
+  const { style, classList, display, type, duration, delay, loop, autoPlay } = component;
   const { onMouseOver, onMouseOut, onClick, onAnimationStart, onCycleComplete, onAnimationEnd } = eventHandlers;
 
-  const [isAnimating, setIsAnimating] = useState(false);
-  const [hasStarted, setHasStarted] = useState(autoStart);
-  const [isInViewport, setIsInViewport] = useState(null);
+  const [isStopped, setIsStopped] = useState(!autoPlay);
 
   const styles = useStyles(style, duration, delay, loop);
+  const isInViewport = useObserver(elRef, display);
+  const animated = !isStopped && isInViewport;
 
   const handleAnimationEnd = useCallback(() => {
-    setHasStarted(false);
-    setIsAnimating(false);
+    setIsStopped(true);
+
     onAnimationEnd();
   }, []);
 
-  useEffect(() => {
-    const ref = elRef.current;
-
-    if (!ref) {
-      return;
-    }
-
-    const observer = new IntersectionObserver(([entry]) => setIsInViewport(entry.isIntersecting), { threshold: 0.5 });
-
-    observer.observe(ref);
-
-    return () => observer.unobserve(ref);
-  }, [elRef, display]);
-
-  useEffect(() => {
-    if (isInViewport && hasStarted) {
-      setIsAnimating(true);
-    } else {
-      setIsAnimating(false);
-    }
-  }, [isInViewport, hasStarted]);
-
-  component.play = () => {
-    setIsAnimating(true);
-    setHasStarted(true);
-  }
-
-  component.stop = () => {
-    setIsAnimating(false);
-    setHasStarted(false);
-  }
+  component.play = () => setIsStopped(false);
+  component.stop = () => setIsStopped(true);
 
   if (!display) {
     return null;
@@ -70,7 +40,7 @@ export default function AnimatedBoxComponent({ component, elRef, eventHandlers, 
       onAnimationIteration={ onCycleComplete }
       onAnimationEnd={ handleAnimationEnd }>
 
-      <div className={ cn('animated-box-content', { animated: isAnimating }, isAnimating && type) }>
+      <div className={ cn('animated-box-content', { animated }, animated && type) }>
         { pods.animatedBoxContent.render() }
       </div>
     </div>
@@ -78,15 +48,26 @@ export default function AnimatedBoxComponent({ component, elRef, eventHandlers, 
 }
 
 function useStyles(style, duration, delay, loop) {
-  return useMemo(() => {
-    const animDuration = duration > 0 ? `${ duration }s` : DEFAULT_ONE;
-    const animDelay = delay > 0 ? `${ delay }s` : DEFAULT_ZERO;
-    const animCount = loop > 0 ? loop : INFINITE;
-
-    return {
+  return useMemo(() => ({
     ...style,
-    '--animate-duration'       : animDuration,
-    '--animate-delay'          : animDelay,
-    '--animate-iteration-count': animCount,
-  }}, [style, duration, delay, loop]);
+    '--animate-duration'       : duration > 0 ? `${ duration }ms` : ONE_SECOND,
+    '--animate-delay'          : delay > 0 ? `${ delay }ms` : 0,
+    '--animate-iteration-count': loop > 0 ? loop : INFINITE,
+  }), [style, duration, delay, loop]);
+}
+
+function useObserver(ref, display) {
+  const [isInViewport, setIsInViewport] = useState(false);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(([entry]) => setIsInViewport(entry.isIntersecting), { threshold: 0.5 });
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => ref.current && observer.unobserve(ref.current);
+  }, [ref, display]);
+
+  return isInViewport;
 }
