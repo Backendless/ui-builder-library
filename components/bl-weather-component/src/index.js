83426fcaeee2bbc1c8de731d14d36d2f14643fc2
Oksana Dmytriieva <46996947+Fialkaksu@users.noreply.github.com>
2023-06-02 12:12:11 +0300

    added additional parameters to control the display of data (#555)

--- components/bl-weather-component/src/index.js
+++ components/bl-weather-component/src/index.js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 
 import ReactWeather, { useOpenWeather } from './lib/weather';
 
@@ -11,10 +11,15 @@ const UNITS = {
 };
 
 export default function WeatherComponent({ component, elRef, settings }) {
-  const { classList, style, display, currentLocation, location, label, forecast, unit, theme, lang, } = component;
+  const {
+    classList, style, display, currentLocation, location, label, weatherInfo, date,
+    temperature, desc, wind, humidity, weatherIcon, forecast, unit, theme, lang,
+  } = component;
   const { API_KEY } = settings;
 
   const [options, setOptions] = useState({ key: API_KEY, lat: null, lon: null, lang, unit });
+
+  const styles = useStyles(style, weatherInfo, date, temperature, desc, wind, humidity, weatherIcon);
 
   useEffect(async () => {
     const usedLocation = currentLocation ? await BackendlessUI.Navigator.getCurrentGeolocation() : location;
@@ -31,7 +36,7 @@ export default function WeatherComponent({ component, elRef, settings }) {
   }
 
   return (
-    <div ref={ elRef } className={ cn('bl-customComponent-weather', classList) } style={ style }>
+    <div ref={ elRef } className={ cn('bl-customComponent-weather', classList) } style={ styles }>
       { options?.lat && options.lon && (
         <Weather options={ options } locationLabel={ label } showForecast={ forecast } theme={ theme } lang={ lang } />
       ) }
@@ -55,4 +60,21 @@ function Weather({ options, locationLabel, showForecast, theme, lang }) {
       theme={ theme }
     />
   );
+}
+
+function useStyles(style, weatherInfo, date, temperature, desc, wind, humidity, weatherIcon) {
+  return useMemo(() => ({
+    ...style,
+    '--weather-info-width'       : weatherInfo && !weatherIcon ? '100%' : '60%',
+    '--weather-icon-width'       : weatherIcon && !weatherInfo ? '100%' : '40%',
+    '--weather-info-display'     : weatherInfo ? 'block' : 'none',
+    '--weather-icon-display'     : weatherIcon ? 'flex' : 'none',
+    '--today-date-display'       : date ? 'block' : 'none',
+    '--today-hr-first-display'   : temperature || desc ? 'block' : 'none',
+    '--today-temperature-display': temperature ? 'block' : 'none',
+    '--today-desc-display'       : desc ? 'block' : 'none',
+    '--today-hr-last-display'    : wind || humidity ? 'block' : 'none',
+    '--today-wind-display'       : wind ? 'block' : 'none',
+    '--today-humidity-display'   : humidity ? 'block' : 'none',
+  }), [style, weatherInfo, date, temperature, desc, wind, humidity, weatherIcon]);
 }
