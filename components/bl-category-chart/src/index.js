4f21f28d739eab585a786e0110cfe5ec12e61214
Fialkaksu <fialkaksu1984@gmail.com>
2023-07-11 12:14:19 +0300

    library updated to the last version, code refactored, readme updated

--- components/bl-category-chart/src/index.js
+++ components/bl-category-chart/src/index.js
@@ -1,32 +1,28 @@
-import { useEffect, useRef } from 'react';
-
+import { useEffect, useMemo, useRef } from 'react';
 import Chart from './chartjs';
 
 const { cn } = BackendlessUI.CSSUtils;
 
 export default function CategoryChartComponent({ component, elRef }) {
   const {
-    classList, display, style, disabled, height, width, type,
-    chartTitleVisibility, chartTitle, chartTitleFontSize, chartBackgroundColor,
-    yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth, labels, datasets, options
+    classList, display, style, disabled, height, width, type, titleVisibility, title, titleFontSize, backgroundColor,
+    legendVisibility, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth, labels, datasets, options
   } = component;
 
   const chartRef = useRef(null);
-
-  useChart(chartRef, {
-    display,
-    type,
-    labels,
-    datasets,
+  const chartConfig = useChartConfig({
     options,
-    chartTitle,
-    chartTitleFontSize,
-    chartTitleVisibility,
+    legendVisibility,
+    titleVisibility,
+    title,
+    titleFontSize,
     yGridLineVisibility,
     xGridLineVisibility,
     gridLinesColor,
-    gridLinesWidth
+    gridLinesWidth,
   });
+
+  useChart(chartRef, display, type, labels, datasets, chartConfig);
 
   const styles = { ...style, width, height };
 
@@ -39,104 +35,83 @@ export default function CategoryChartComponent({ component, elRef }) {
       ref={ elRef }
       style={ styles }
       className={
-        cn("bl-customComponent-categoryChart", classList, { "bl-customComponent-categoryChart--disabled": disabled })
+        cn('bl-customComponent-categoryChart', classList, { 'bl-customComponent-categoryChart--disabled': disabled })
       }>
-      <canvas ref={ chartRef } style={{ backgroundColor: chartBackgroundColor, width: "100%", height: "100%" }} />
+      <canvas ref={ chartRef } style={{ backgroundColor, width: '100%', height: '100%' }}/>
     </div>
   );
 }
 
-const useChart = (chartRef, props) => {
-  const {
-    display, type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
-    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
-  } = props;
-  const chartInstance = useRef();
+const useChart = (chartRef, display, type, labels, datasets, chartConfig) => {
+  const chartInstance = useRef(null);
 
   useEffect(() => {
     if (!display) {
-      return null;
+      return;
     }
 
-    chartInstance.current = new Chart(chartRef.current, {
-      type,
-      data: { labels, datasets },
-      options: {
-        ...options,
-        plugins: {
-          title: {
-            display: chartTitleVisibility,
-            text: chartTitle,
-            font: {
-              size: chartTitleFontSize
-            }
-          }
-        },
-        scales: {
-          y: {
-            grid: {
-              display: yGridLineVisibility,
-              color: gridLinesColor,
-              lineWidth: gridLinesWidth
-            }
-          },
-          x: {
-            grid: {
-              display: xGridLineVisibility,
-              color: gridLinesColor,
-              lineWidth: gridLinesWidth
-            },
-          }
-        }
+    if (chartInstance.current) {
+      chartInstance.current.data = { labels, datasets };
+      chartInstance.current.options = chartConfig;
+      chartInstance.current.update();
+    } else {
+      chartInstance.current = new Chart(chartRef.current, {
+        type,
+        data: { labels, datasets },
+        options: chartConfig,
+      });
+    }
+
+    return () => {
+      if (chartInstance.current) {
+        chartInstance.current.destroy();
+        chartInstance.current = null;
       }
-    });
-
-    return () => chartInstance.current.destroy();
-  }, [display]);
-
-  useEffect(() => {
-    if (!display) {
-      return null;
-    }
-
-    const { current: chart } = chartInstance;
-
-    if (chart) {
-      chart.type = type;
-      chart.options = {
-        ...options,
-        plugins: {
-          title: {
-            display: chartTitleVisibility,
-            text: chartTitle,
-            font: {
-              size: chartTitleFontSize
-            }
-          }
-        },
-        scales: {
-          y: {
-            grid: {
-              display: yGridLineVisibility,
-              color: gridLinesColor,
-              lineWidth: gridLinesWidth
-            }
-          },
-          x: {
-            grid: {
-              display: xGridLineVisibility,
-              color: gridLinesColor,
-              lineWidth: gridLinesWidth
-            }
-          }
-        }
-      };
-      Object.assign(chart.data, { labels, datasets });
-
-      chart.update();
-    }
-  }, [
-    type, labels, datasets, options, chartTitleVisibility, chartTitleFontSize,
-    chartTitle, yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth
-  ]);
+    };
+  }, [chartRef.current, display, type, labels, datasets, chartConfig]);
 };
+
+const useChartConfig = ({
+  options,
+  legendVisibility,
+  titleVisibility,
+  title,
+  titleFontSize,
+  yGridLineVisibility,
+  xGridLineVisibility,
+  gridLinesColor,
+  gridLinesWidth,
+}) => useMemo(() => ({
+  ...options,
+  responsive: true,
+  maintainAspectRatio: false,
+  plugins: {
+    legend: {
+      display: legendVisibility,
+    },
+    title: {
+      display: titleVisibility,
+      text: title,
+      font: {
+        size: titleFontSize,
+      },
+    },
+  },
+  scales: {
+    y: {
+      grid: {
+        display: yGridLineVisibility,
+        color: gridLinesColor,
+        lineWidth: gridLinesWidth,
+      },
+    },
+    x: {
+      grid: {
+        display: xGridLineVisibility,
+        color: gridLinesColor,
+        lineWidth: gridLinesWidth,
+      },
+    },
+  },
+}), [options, legendVisibility, titleVisibility, title, titleFontSize,
+  yGridLineVisibility, xGridLineVisibility, gridLinesColor, gridLinesWidth,]);
