b1e60fd3c8b84d8dfc0de1e1a9613eae08086081
Fialkaksu <fialkaksu1984@gmail.com>
2023-08-15 11:58:01 +0300

    minor edits in code, styles

--- components/bl-sweet-alert/src/index.js
+++ components/bl-sweet-alert/src/index.js
@@ -1,4 +1,5 @@
 import { useCallback, useEffect, useMemo, useRef } from 'react';
+
 import Swal from './lib/sweetalert2';
 
 const { cn } = BackendlessUI.CSSUtils;
@@ -8,8 +9,6 @@ export default function SweetAlertComponent({ component, eventHandlers, elRef })
     style, classList, display, position, title, text, icon, iconColor, footer, grow, backdrop,
     allowOutsideClick, allowEscapeKey, allowEnterKey, buttonsSettings, additionalSettings,
   } = component;
-
-  const { onConfirm, onDeny, onDismiss } = eventHandlers;
 
   const swalInstanceRef = useRef(null);
 
@@ -21,25 +20,12 @@ export default function SweetAlertComponent({ component, eventHandlers, elRef })
 
   const showAlert = useCallback(() => {
     if (elRef.current && !swalInstanceRef.current) {
-      swalInstanceRef.current = createAlert({ target: elRef.current, ...options });
-      swalInstanceRef.current.then((response) => {
-        if (response.isConfirmed && onConfirm) {
-          onConfirm({ response });
-        }
-
-        if (response.isDismissed && onDismiss) {
-          onDismiss({ response });
-        }
-
-        if (response.isDenied && onDeny) {
-          onDeny({ response });
-        }
-      }).finally(() => swalInstanceRef.current = null);
+      createAlert(elRef, swalInstanceRef, options, eventHandlers);
     }
-  }, [elRef.current, options, onConfirm, onDismiss, onDeny]);
+  }, [elRef.current, options, eventHandlers]);
 
   useEffect(() => {
-    if (swalInstanceRef.current) {
+    if (swalInstanceRef.current && options) {
       updateAlert(swalInstanceRef, options);
     }
   }, [swalInstanceRef.current, options]);
@@ -52,22 +38,31 @@ export default function SweetAlertComponent({ component, eventHandlers, elRef })
   }
 
   return (
-    <div className={ cn('bl-customComponent-sweet-alert', classList) } ref={ elRef } style={ style } />
+    <div className={ cn('bl-customComponent-sweet-alert', classList) } ref={ elRef } style={ style }/>
   );
 }
 
-const createAlert = (options) => Swal.fire(options);
+const createAlert = (elRef, swalRef, options, eventHandlers) => {
+  const { onConfirm, onDeny, onDismiss } = eventHandlers;
+
+  swalRef.current = Swal.fire({ target: elRef.current, ...options });
+  swalRef.current.then(response => {
+    const { isConfirmed, isDismissed, isDenied } = response;
+
+    isConfirmed && onConfirm({ response });
+    isDismissed && onDismiss({ response });
+    isDenied && onDeny({ response });
+  }).finally(() => swalRef.current = null);
+};
 
 const updateAlert = (swalRef, options) => {
-  if (swalRef.current && options) {
-    const updateOptions = Object.keys(options).reduce((acc, key) => {
-      if (Swal.isUpdatableParameter(key)) {
-        acc[key] = options[key];
-      }
+  const updateOptions = Object.keys(options).reduce((acc, key) => {
+    if (Swal.isUpdatableParameter(key)) {
+      acc[key] = options[key];
+    }
 
-      return acc;
-    }, {});
+    return acc;
+  }, {});
 
-    swalRef.current.update({ ...updateOptions });
-  }
+  swalRef.current.update({ ...updateOptions });
 };
