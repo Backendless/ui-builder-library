0d3aeab893a41846f03006f03069804c588623d7
pavlostasiv <pavlostasiv@gmail.com>
2023-08-18 13:43:00 +0300

    reworked skeleton with using pod container, updated README.md

--- components/bl-skeleton/src/index.js
+++ components/bl-skeleton/src/index.js
@@ -1,16 +1,25 @@
 const { cn } = BackendlessUI.CSSUtils;
 
-export default function Skeleton({ component }) {
-  const { style, display, classList, variant, width, height, animation } = component;
+export default function Skeleton({ component, pods }) {
+  const { style, display, classList, isContentVisible, variant, width, height, animation } = component;
+  const skeletonContent = pods['skeletonContent'];
 
   if (!display) {
     return null;
   }
 
+  if (!isContentVisible) {
+    return (
+      <div
+        className={ cn('bl-customComponent-skeleton', variant, animation, classList) }
+        style={{ ...style, width: width || '100%', height: height || '100%' }}>
+      </div>
+    );
+  }
+
   return (
-    <div
-      className={ cn('bl-customComponent-skeleton',  variant, animation, classList) }
-      style={ { ...style, width: width || '100%', height: height || '100%' } }>
-    </div>
+    <>
+      { skeletonContent.render() }
+    </>
   );
 }
