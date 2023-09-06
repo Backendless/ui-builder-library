bfca0c5775e5cce892266393a4cb79541064a091
pavlo <pavlostasiv@gmail.com>
2023-09-06 14:34:38 +0300

    revert pods

--- components/bl-skeleton/src/index.js
+++ components/bl-skeleton/src/index.js
@@ -1,21 +1,16 @@
 const { cn } = BackendlessUI.CSSUtils;
 
-export default function Skeleton({ component, pods }) {
-  const { style, display, classList, isContentVisible, variant, width, height, animation } = component;
-  const skeletonContent = pods['skeletonContent'];
+export default function Skeleton({ component }) {
+  const { style, display, classList, variant, width, height, animation } = component;
 
   if (!display) {
     return null;
   }
 
-  if (!isContentVisible) {
-    return (
-      <div
-        className={ cn('bl-customComponent-skeleton', variant, animation, classList) }
-        style={{ ...style, width: width || '100%', height: height || '100%' }}>
-      </div>
-    );
-  }
-
-  return skeletonContent.render();
+  return (
+    <div
+      className={ cn('bl-customComponent-skeleton',  variant, animation, classList) }
+      style={ { ...style, width: width || '100%', height: height || '100%' } }>
+    </div>
+  );
 }
