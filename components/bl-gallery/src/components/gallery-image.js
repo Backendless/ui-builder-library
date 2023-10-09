00039b7ec0909c1003f69343cffe16df03fbe8f5
Dima <vakyla98@gmail.com>
2023-10-10 01:10:27 +0300

    test ci

--- components/bl-gallery/src/components/gallery-image.js
+++ components/bl-gallery/src/components/gallery-image.js
@@ -1,11 +1,7 @@
 export function GalleryImage({ url, height }) {
 
+  console.log(123)
   return (
-    <img
-      src={ url }
-      alt="gallery item"
-      style={{ height: height }}
-      className="image"
-    />
+    <img src={ url + 1232222222222222222222222222 }     alt="gallery item"     style={{ height: height }}      className="image"    />
   );
 }
