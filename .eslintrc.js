7082fe15e832e0c0963fae027dfb394cae8c6894
Dima <vakyla98@gmail.com>
2023-10-09 23:15:03 +0300

    setup eslint

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  'extends': 'eslint-config-backendless',
+  'rules'  : {
+    'simple-import-sort/imports': [
+      'warn',
+      {
+        'groups': [
+          /* node_modules */
+          [
+            '^react',
+            '^prop-types',
+            '^@?\\w',
+          ],
+
+          /* External libraries files */
+          [
+            '^\\./lib/(?=.*/)(?!/?$)',
+            '^\\./lib/(?!/?$)',
+            '^\\./lib/?$',
+          ],
+
+          /* Relative imports */
+          [
+            '^\\./(?=.*/)(?!/?$)',
+            '^\\.(?!/?$)',
+            '^\\./?$',
+          ],
+        ],
+      },
+    ],
+
+    'semi'   : [1, 'always'],
+    'max-len': ['warn', {
+      'code'         : 120,
+      'ignoreStrings': true,
+    }],
+  },
+  'globals': {
+    'React'        : true,
+    'ReactDOM'     : true,
+    'BackendlessUI': true,
+    'Backendless'  : true,
+  },
+};
