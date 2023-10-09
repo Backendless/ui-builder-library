07a891227fca5668b980e9504dd8757fe6ba155f
Zahar Mychka <56361878+Saharij@users.noreply.github.com>
2023-10-09 21:26:41 +0300

    (Support)[Autocomplete] Implement setting selected option (#806)
    * (Support)[Autocomplete] Implement setting selected option
    
    * created one file for all validators, renamed functions validators
    
    ---------
    
    Co-authored-by: Zakhar Mychka <zakharii.mychka@plus4u.net>

--- components/bl-autocomplete/src/helpers/index.js
+++ components/bl-autocomplete/src/helpers/index.js
@@ -1,4 +1,4 @@
-export * from './validate';
+export * from './validators';
 export * from './use-on-click-outside';
 export * from './use-filtered-options';
 export * from './use-options-placement';
