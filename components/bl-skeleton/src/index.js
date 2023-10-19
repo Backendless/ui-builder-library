3484224573a0a0a82e382f63542bf0e72c8142a1
pavlo <pavlostasiv@gmail.com>
2023-10-19 11:07:55 +0300

    fixed validMargin when the margin is undefined

--- components/bl-skeleton/src/index.js
+++ components/bl-skeleton/src/index.js
@@ -5,7 +5,7 @@ const { cn, normalizeDimensionValue } = BackendlessUI.CSSUtils;
 export default function Skeleton({ component }) {
   const { style, display, classList, variant, width, height, animation, margin } = component;
 
-  const validMargin = useMemo(() => margin.split(' ').map(item => normalizeDimensionValue(item)).join(' '), [margin]);
+  const validMargin = useMemo(() => margin?.split(' ').map(item => normalizeDimensionValue(item)).join(' '), [margin]);
 
   if (!display) {
     return null;
