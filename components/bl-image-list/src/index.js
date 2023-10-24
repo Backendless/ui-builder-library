aeda98ecb304df40d892299e366128f3fe833674
Fialkaksu <fialkaksu1984@gmail.com>
2023-10-24 13:40:24 +0300

    fix lint

--- components/bl-image-list/src/index.js
+++ components/bl-image-list/src/index.js
@@ -9,7 +9,10 @@ const TYPE_QUILTED = 'quilted';
 const IMAGE_HEIGHT_REDUCTION_FACTOR = 0.7;
 
 export default function ImageListComponent({ component, eventHandlers, elRef }) {
-  const { style, classList, display, itemData, listVariant, cols, rowHeight, gap, showTitleBar, titleBarPosition, titleBarHeight } = component;
+  const {
+    style, classList, display, itemData, listVariant, cols, rowHeight,
+    gap, showTitleBar, titleBarPosition, titleBarHeight
+  } = component;
 
   const [items, setItems] = useState([]);
   const imageStyle = useCalculatedImageStyle(showTitleBar, rowHeight, titleBarHeight, titleBarPosition);
