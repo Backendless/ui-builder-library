ebe6f217c9272f5760884b003766f12f4c3eb720
Zakhar Mychka <zakharii.mychka@plus4u.net>
2023-11-03 15:30:31 +0200

    MARKET-2318 [Rating Reviews] Rename the Handler.

--- components/bl-rating-reviews/src/index.js
+++ components/bl-rating-reviews/src/index.js
@@ -6,9 +6,9 @@ import { ViewsMap } from './views';
 const { cn, normalizeDimensionValue } = BackendlessUI.CSSUtils;
 
 export default function RatingReviews({ component, eventHandlers, elRef }) {
-  const { ratingData, color, iconType, iconSize, width, view, localizations, style, classList, display } = component;
+  const { ratingReviewsData, color, iconType, iconSize, width, view, localizations, style, classList, display } = component;
 
-  const processedData = useMemo(() => prepareReviewData(ratingData, color), [ratingData, color]);
+  const processedData = useMemo(() => prepareReviewData(ratingReviewsData, color), [ratingReviewsData, color]);
   const totalReviews = useMemo(() => calculateTotalReviews(processedData), [processedData]);
   const average = useMemo(() => calculateAverage(processedData), [processedData]);
   const maxValue = processedData.length;
